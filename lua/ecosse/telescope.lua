af7d94a207e7760770f4cc00b873d20aa519b8ed
ecosse3 <theecosse@gmail.com>
2021-03-04 00:27:32 +0100

    nvim(plugins): add telescope lua config

---
+++ lua/ecosse/telescope.lua
@@ -0,0 +1,40 @@
+local actions = require('telescope.actions')
+require('telescope').setup {
+    defaults = {
+        vimgrep_arguments = {
+          'rg',
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+          '--smart-case'
+        },
+        file_sorter = require('telescope.sorters').get_fzy_sorter,
+        prompt_prefix = ' 🔍 ',
+        color_devicons = true,
+
+        prompt_position = "top",
+        sorting_strategy = "ascending",
+
+        file_previewer   = require('telescope.previewers').vim_buffer_cat.new,
+        grep_previewer   = require('telescope.previewers').vim_buffer_vimgrep.new,
+        qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+
+        mappings = {
+            i = {
+                ["<C-x>"] = false,
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous
+            },
+        }
+    },
+    extensions = {
+        fzy_native = {
+            override_generic_sorter = false,
+            override_file_sorter = true,
+        }
+    }
+}
+
+require('telescope').load_extension('fzy_native')
