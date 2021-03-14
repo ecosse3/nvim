23b9b6ad83924356269dbf9f8d3a5a0b782a443d
ecosse3 <luk.kurpiewski@gmail.com>
2021-03-15 00:52:55 +0100

    nvim(lua/plugins): disable lightbulb & change saga bulb icon

--- lua/plugins/saga.lua
+++ lua/plugins/saga.lua
@@ -2,12 +2,12 @@ local saga = require 'lspsaga'
 
 
 saga.init_lsp_saga {
-  code_action_icon = '',
+  code_action_icon = '💡',
   code_action_prompt = {
     enable = true,
     sign = true,
     sign_priority = 20,
-    virtual_text = true,
+    virtual_text = false,
   },
   code_action_keys = { quit = {'q', '<ESC>'}, exec = '<CR>' },
   border_style = 2,
