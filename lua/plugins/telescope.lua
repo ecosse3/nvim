1b57e9daa9412370ce174d70af3325c56905459d
ecosse3 <luk.kurpiewski@gmail.com>
2021-04-24 01:39:19 +0200

    nvim(plugins): don't override generic sorter in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -32,7 +32,7 @@ require('telescope').setup {
     },
     extensions = {
         fzf = {
-            override_generic_sorter = true,
+            override_generic_sorter = false,
             override_file_sorter = true,
             case_mode = "smart_case",
         }
