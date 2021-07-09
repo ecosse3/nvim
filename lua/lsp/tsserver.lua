fc4986f844395ade26bb83f302282d458c96aac3
ecosse3 <luk.kurpiewski@gmail.com>
2021-07-09 19:36:06 +0200

    plugins: configure lsp_signature inside tsserver

--- lua/lsp/tsserver.lua
+++ lua/lsp/tsserver.lua
@@ -6,9 +6,64 @@ require'lspconfig'.tsserver.setup({
     client.resolved_capabilities.document_formatting = false
     on_attach(client)
 
-    require'nvim-lsp-ts-utils'.setup {}
+    require'lsp_signature'.on_attach({
+      bind = false, -- This is mandatory, otherwise border config won't get registered.
+                   -- If you want to hook lspsaga or other signature handler, pls set to false
+      doc_lines = 2, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
+                     -- set to 0 if you DO NOT want any API comments be shown
+                     -- This setting only take effect in insert mode, it does not affect signature help in normal
+                     -- mode, 10 by default
+
+      floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
+      fix_pos = false,  -- set to true, the floating window will not auto-close until finish all parameters
+      hint_enable = false, -- virtual hint enable
+      hint_prefix = "🐼 ",  -- Panda for parameter
+      hint_scheme = "String",
+      use_lspsaga = true,  -- set to true if you want to use lspsaga popup
+      hi_parameter = "Search", -- how your parameter will be highlight
+      max_height = 12, -- max height of signature floating_window, if content is more than max_height, you can scroll down
+                       -- to view the hiding contents
+      max_width = 120, -- max_width of signature floating_window, line will be wrapped if exceed max_width
+      handler_opts = {
+        border = "single"   -- double, single, shadow, none
+      },
+      extra_trigger_chars = {} -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
+    })
+
+    local ts_utils = require("nvim-lsp-ts-utils")
+
+    ts_utils.setup {
+        debug = false,
+        disable_commands = false,
+        enable_import_on_completion = false,
+        import_all_timeout = 5000, -- ms
+
+        -- eslint
+        eslint_enable_code_actions = true,
+        eslint_enable_disable_comments = true,
+        eslint_bin = 'eslint_d',
+        eslint_config_fallback = nil,
+        eslint_enable_diagnostics = true,
+
+        -- formatting
+        enable_formatting = true,
+        formatter = 'prettier',
+        formatter_config_fallback = nil,
+
+        -- parentheses completion
+        complete_parens = false,
+        signature_help_in_parens = false,
+
+        -- update imports on file move
+        update_imports_on_move = true,
+        require_confirmation_on_move = true,
+        watch_dir = nil,
+    }
+
+    ts_utils.setup_client(client)
 
     vim.api.nvim_buf_set_keymap(bufnr, "n", "<Leader>co", ":TSLspOrganize<CR>", {silent = true})
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "qq", ":TSLspFixCurrent<CR>", {silent = true})
     vim.api.nvim_buf_set_keymap(bufnr, "n", "<Leader>cR", ":TSLspRenameFile<CR>", {silent = true})
     vim.api.nvim_buf_set_keymap(bufnr, "n", "<Leader>ci", ":TSLspImportAll<CR>", {silent = true})
   end
