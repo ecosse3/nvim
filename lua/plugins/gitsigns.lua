8af6d32c6a5b9af280884865b5ff65a6d387bbb3
ecosse3 <luk.kurpiewski@gmail.com>
2021-09-08 00:09:02 +0200

    plugins: update gitsigns config

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -6,7 +6,10 @@
       topdelete    = {hl = 'GitGutterDelete', text = '‾', numhl='GitSignsDeleteNr'},
       changedelete = {hl = 'GitGutterChange', text = '~', numhl='GitSignsChangeNr'},
     },
-    numhl = false,
+    signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+    numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+    linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+    word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
     keymaps = {
       -- Default keymap options
       noremap = true,
@@ -19,11 +22,36 @@
       ['n <leader>ghu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
       ['n <leader>ghr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
       ['n <leader>ghp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-      ['n <leader>ghb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
+      ['n <leader>ghb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
     },
     watch_index = {
-      interval = 700
+      interval = 700,
+      follow_files = true
+    },
+    attach_to_untracked = true,
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+      delay = 700,
+    },
+    current_line_blame_formatter_opts = {
+      relative_time = false
     },
     sign_priority = 6,
+    update_debounce = 100,
     status_formatter = nil, -- Use default
+    max_file_length = 40000,
+    preview_config = {
+      -- Options passed to nvim_open_win
+      border = 'single',
+      style = 'minimal',
+      relative = 'cursor',
+      row = 0,
+      col = 1
+    },
+    use_internal_diff = true,  -- If vim.diff or luajit is present
+    yadm = {
+      enable = false
+    },
   }
