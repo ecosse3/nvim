fd56c02e3cd9e7702d58ed5927e8c57e0589c433
ecosse3 <luk.kurpiewski@gmail.com>
2021-09-13 18:23:34 +0200

    plugins: add nvim-gps

---
+++ lua/plugins/gps.lua
@@ -0,0 +1,14 @@
+require("nvim-gps").setup({
+	icons = {
+		["class-name"] = ' ',      -- Classes and class-like objects
+		["function-name"] = ' ',   -- Functions
+		["method-name"] = ' '      -- Methods (functions inside class-like objects)
+	},
+	-- Disable any languages individually over here
+	-- Any language not disabled here is enabled by default
+	languages = {
+		-- ["bash"] = false,
+		-- ["go"] = false,
+	},
+	separator = ' > ',
+})
