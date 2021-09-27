6db64898a2e0d781cda04859faa73aa2dbc3a312
ecosse3 <luk.kurpiewski@gmail.com>
2021-09-27 22:44:39 +0200

    plugins: add dashboard.nvim config

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,12 +1,23 @@
 local icons = EcoVim.icons
 
+vim.g.dashboard_custom_header = {
+' ██   ██                  ██             ',
+'░██  ██                  ░██      ██   ██',
+'░██ ██   ██    ██  ██████░██  ██ ░░██ ██ ',
+'░████   ░██   ░██ ██░░░░ ░██ ██   ░░███  ',
+'░██░██  ░░██ ░██ ░░█████ ░████     ░██   ',
+'░██░░██  ░░████   ░░░░░██░██░██    ██    ',
+'░██ ░░██  ░░██    ██████ ░██░░██  ██     ',
+'░░   ░░    ░░    ░░░░░░  ░░  ░░  ░░      ',
+}
+
+vim.g.dashboard_session_directory = '~/.config/nvim/sessions'
 vim.g.dashboard_default_executive = 'telescope'
 vim.g.dashboard_custom_section = {
   a = {description = {icons.fileNoBg .. 'Find File          '}, command = 'Telescope find_files hidden=true'},
   b = {description = {icons.fileCopy .. 'Recently Used Files'}, command = 'Telescope oldfiles hidden=true'},
   c = {description = {icons.timer .. 'Load Last Session  '}, command = 'SessionLoad'},
   d = {description = {icons.t .. 'Find Word          '}, command = 'Telescope live_grep'},
-  e = {description = {icons.container .. 'Packer Sync        '}, command = 'PackerSync'},
-  f = {description = {icons.container .. 'Packer Install     '}, command = 'PackerInstall'}
-  -- e = {description = {icons.ribbonNoBg .. 'Marks              '}, command = 'Telescope marks'}
+  e = {description = {icons.container .. 'Sync Plugins        '}, command = 'PackerSync'},
+  f = {description = {icons.container .. 'Install Plugins     '}, command = 'PackerInstall'}
 }
