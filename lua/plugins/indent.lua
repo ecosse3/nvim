74728e0a12b8bcd6efeab88040b2a00b00eeb2ab
ecosse3 <luk.kurpiewski@gmail.com>
2021-09-27 21:32:06 +0200

    plugins: update indent config to exclude startify, packer, trouble, nvimtree and help

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -5,7 +5,8 @@ vim.opt.listchars = {
 
 require("indent_blankline").setup {
   char = "|",
-  buftype_exclude = {"terminal", "startify"},
+  buftype_exclude = {"terminal", "telescope"},
+  filetype_exclude = {"help", "startify", "packer", "NvimTree", "Trouble"},
   space_char_blankline = " ",
   show_current_context = true,
   show_end_of_line = true,
