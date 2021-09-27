6781301389d5eead4b9b767589bd3ca674be9593
ecosse3 <luk.kurpiewski@gmail.com>
2021-09-27 21:19:24 +0200

    plugins: update cmp settings

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,6 +3,12 @@ local cmp = require('cmp')
 local lspkind = require('lspkind')
 local tabnine = require('cmp_tabnine.config')
 
+local source_mapping = {
+  buffer      = "[B]",
+  nvim_lsp    = "[LSP]",
+  cmp_tabnine = "[TN]",
+}
+
 cmp.setup {
   snippet = {
     expand = function(args)
@@ -38,29 +44,35 @@ cmp.setup {
   formatting = {
     format = function(entry, vim_item)
       vim_item.kind = lspkind.presets.default[vim_item.kind]
-      vim_item.menu = ({
-        cmp_tabnine = "[TN]",
-        nvim_lsp = "[LSP]",
-        buffer = "[B]",
-      })[entry.source.name]
+      local menu = source_mapping[entry.source.name]
+
+      if entry.source.name == 'cmp_tabnine' then
+				if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
+					menu = menu .. ' ' .. entry.completion_item.data.detail
+				end
+				vim_item.kind = ''
+			end
+
+      vim_item.menu = menu
       return vim_item
     end
   },
 
   -- You should specify your *installed* sources.
   sources = {
-    { name = 'nvim_lsp' },
-    { name = 'buffer' },
-    { name = 'path' },
-    { name = 'calc' },
+    { name = 'nvim_lsp'    },
+    { name = 'buffer'      },
+    { name = 'path'        },
+    { name = 'calc'        },
     { name = 'cmp_tabnine' },
-    { name = 'nvim_lua' },
-    { name = 'vsnip' },
+    { name = 'nvim_lua'    },
+    { name = 'vsnip'       },
   },
 }
 
 tabnine:setup({
-        max_lines = 1000;
-        max_num_results = 3;
-        sort = true;
+        max_lines                = 1000;
+        max_num_results          = 3;
+        sort                     = true;
+        show_prediction_strength = true;
 })
