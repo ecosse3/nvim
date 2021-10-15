afea091f2ddd829a9178f7ed411ad0cb49eb8a80
ecosse3 <luk.kurpiewski@gmail.com>
2021-10-16 01:04:05 +0200

    lsp: add automated installation for CSS LSP

--- lua/lsp/css.lua
+++ lua/lsp/css.lua
@@ -1,2 +1,8 @@
--- npm install -g vscode-css-languageserver-bin
-require'lspconfig'.cssls.setup{}
+local lsp_installer_servers = require'nvim-lsp-installer.servers'
+
+local ok, cssls = lsp_installer_servers.get_server("cssls")
+if ok then
+    if not cssls:is_installed() then
+        cssls:install()
+    end
+end
