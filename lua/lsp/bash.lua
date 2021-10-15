d59dd5eafbd8d4648bfac3e238fa6b73f61274b5
ecosse3 <luk.kurpiewski@gmail.com>
2021-10-16 01:04:51 +0200

    lsp: add automated installation for Bash LSP

---
+++ lua/lsp/bash.lua
@@ -0,0 +1,8 @@
+local lsp_installer_servers = require'nvim-lsp-installer.servers'
+
+local ok, bashls = lsp_installer_servers.get_server("bashls")
+if ok then
+    if not bashls:is_installed() then
+        bashls:install()
+    end
+end
