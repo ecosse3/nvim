aee2cfe6d0109723bc7db8f759595c3f39416d15
ecosse3 <luk.kurpiewski@gmail.com>
2021-10-16 01:04:14 +0200

    lsp: add automated installation for GraphQL LSP

--- lua/lsp/graphql.lua
+++ lua/lsp/graphql.lua
@@ -1,2 +1,8 @@
--- npm install -g graphql-language-service-cli
-require'lspconfig'.graphql.setup{}
+local lsp_installer_servers = require'nvim-lsp-installer.servers'
+
+local ok, graphql = lsp_installer_servers.get_server("graphql")
+if ok then
+    if not graphql:is_installed() then
+        graphql:install()
+    end
+end
