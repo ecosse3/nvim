29e315829d2577ba6ed33c280de9cb90b31e4e90
ecosse3 <luk.kurpiewski@gmail.com>
2021-10-15 18:34:20 +0200

    plugins: change neoscroll options

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -3,8 +3,8 @@ require('neoscroll').setup({
     mappings = {'<C-d>', '<C-f>'},
     hide_cursor = true,          -- Hide cursor while scrolling
     stop_eof = true,             -- Stop at <EOF> when scrolling downwards
-    use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
-    respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+    use_local_scrolloff = true, -- Use the local scope of scrolloff instead of the global scope
+    respect_scrolloff = true,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
     cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
     easing_function = nil,        -- Default easing function
     pre_hook = nil,              -- Function to run before the scrolling animation starts
