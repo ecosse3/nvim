186a294ccad1a5be19547537544d1cdac3488cfe
ecosse3 <luk.kurpiewski@gmail.com>
2021-10-21 00:14:21 +0200

    cmp: change prev_item & next_item mapping

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -36,8 +36,8 @@ cmp.setup {
 
   -- You must set mapping if you want.
   mapping = {
-    ['<C-b>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-k>'] = cmp.mapping.select_prev_item(),
+    ['<C-j>'] = cmp.mapping.select_next_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
