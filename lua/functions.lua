708d13c4638827522f2625d8b47cc5595e597bdd
ecosse3 <luk.kurpiewski@gmail.com>
2021-10-21 00:25:28 +0200

    change nvim_create_augroups function to local

--- lua/functions.lua
+++ lua/functions.lua
@@ -1,4 +1,4 @@
-function nvim_create_augroups(definitions)
+local function nvim_create_augroups(definitions)
     for group_name, definition in pairs(definitions) do
         vim.api.nvim_command('augroup '..group_name)
         vim.api.nvim_command('autocmd!')
