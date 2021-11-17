5f2bb4e6f7a94794dca35bc54c7413de4d07e71a
ecosse3 <luk.kurpiewski@gmail.com>
2021-11-17 01:15:04 +0100

    plugins: remove completion cmp setup from autopairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,7 +1,1 @@
 require('nvim-autopairs').setup()
-
-require("nvim-autopairs.completion.cmp").setup({
-  map_cr = true, --  map <CR> on insert mode
-  map_complete = true, -- it will auto insert `(` after select function or method item
-  auto_select = true -- automatically select the first item
-})
