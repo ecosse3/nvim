22b02b74fdd4fdadb2da2e5e56c113e62e959190
ecosse3 <luk.kurpiewski@gmail.com>
2021-11-23 23:09:51 +0100

    lsp: remove deprecated capabilities UI functionality

--- lua/lsp/config.lua
+++ lua/lsp/config.lua
@@ -4,6 +4,3 @@ for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
-
--- UI just like `:LspInfo` to show which capabilities each attached server has
-vim.api.nvim_command("command! LspCapabilities lua require'lsp/capabilities'()")
