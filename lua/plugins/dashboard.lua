43140b668554db23a1c20d3a3e47fdfc86575952
ecosse3 <luk.kurpiewski@gmail.com>
2021-11-26 19:15:22 +0100

    plugins: reorder dashboard menu

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -16,9 +16,9 @@ vim.g.dashboard_session_directory = '~/.config/nvim/sessions'
 vim.g.dashboard_default_executive = 'telescope'
 vim.g.dashboard_custom_section = {
   a = {description = {icons.fileNoBg ..     'Find File          '}, command = 'Telescope find_files hidden=true'},
-  b = {description = {icons.fileCopy ..     'Recents            '}, command = 'Telescope oldfiles hidden=true'},
-  c = {description = {icons.timer ..        'Load Last Session  '}, command = 'SessionLoad'},
-  d = {description = {icons.t ..            'Find Word          '}, command = 'Telescope live_grep'},
+  b = {description = {icons.t ..            'Find Word          '}, command = 'Telescope live_grep'},
+  c = {description = {icons.fileCopy ..     'Recents            '}, command = 'Telescope oldfiles hidden=true'},
+  d = {description = {icons.timer ..        'Load Last Session  '}, command = 'SessionLoad'},
   e = {description = {icons.container ..    'Sync Plugins       '}, command = 'PackerSync'},
   f = {description = {icons.container ..    'Install Plugins    '}, command = 'PackerInstall'},
   g = {description = {icons.vim ..          'Settings           '}, command = 'edit $MYVIMRC'},
