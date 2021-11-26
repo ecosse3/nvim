f0fe3a0686e3f7a07d9338e902b981244f4fc303
ecosse3 <luk.kurpiewski@gmail.com>
2021-11-26 19:16:58 +0100

    lsp: move Lua lsp settings to lua.lua file

--- lua/lsp/servers/lua.lua
+++ lua/lsp/servers/lua.lua
@@ -1,3 +1,6 @@
+local M = {}
+
+-- Auto-install
 local lsp_installer_servers = require'nvim-lsp-installer.servers'
 
 local ok, lua = lsp_installer_servers.get_server("sumneko_lua")
@@ -6,3 +9,15 @@ if ok then
         lua:install()
     end
 end
+
+-- Settings
+
+M.settings = {
+  Lua = {
+    diagnostics = {
+      globals = {'vim'}
+    }
+  }
+}
+
+return M
