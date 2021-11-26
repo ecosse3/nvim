3fef732126cf30b02169cbbfd9fd2740c30ed447
ecosse3 <luk.kurpiewski@gmail.com>
2021-11-26 22:47:52 +0100

    lsp: configure settings & capabilities in lsp installer

--- lua/lsp/installer.lua
+++ lua/lsp/installer.lua
@@ -13,6 +13,14 @@ lsp_installer.on_server_ready(function(server)
         capabilities = capabilities,
     }
 
+    if server.name == "bash" then
+      opts.settings = require('lsp.servers.bash').settings
+    end
+
+    if server.name == "cssls" then
+      opts.settings = require('lsp.servers.css').settings
+    end
+
     if server.name == "eslint" then
         opts.on_attach = function (client, bufnr)
             -- neovim's LSP client does not currently support dynamic capabilities registration, so we need to set
@@ -20,27 +28,31 @@ lsp_installer.on_server_ready(function(server)
             client.resolved_capabilities.document_formatting = true
             on_attach(client, bufnr)
         end
-        opts.settings = {
-            format = { enable = true },
-        }
+        opts.settings = require('lsp.servers.eslint').settings
+    end
+
+    if server.name == "html" then
+      opts.capabilities = require('lsp.servers.html').capabilities
+      opts.settings = require('lsp.servers.html').settings
+    end
+
+    if server.name == "jsonls" then
+      opts.settings = require('lsp.servers.json').settings
     end
 
     if server.name == "sumneko_lua" then
-      opts.settings = {
-        Lua = {
-          diagnostics = {
-            globals = {'vim'}
-          }
-        }
-      }
+      opts.settings = require('lsp.servers.lua').settings
     end
+
 
     -- (How to) Customize the options passed to the server
     -- if server.name == "tsserver" then
     --     opts.root_dir = function() ... end
+    --     opts.on_attach = function(client, bufnr) ... end
     -- end
 
     -- This setup() function is exactly the same as lspconfig's setup function (:help lspconfig-quickstart)
     server:setup(opts)
     vim.cmd [[ do User LspAttachBuffers ]]
+    require('lsp.config')
 end)
