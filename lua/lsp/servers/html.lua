905d13ed4df2dc6b8760cb74b8b0ee94e7da80fb
ecosse3 <luk.kurpiewski@gmail.com>
2021-11-26 19:16:46 +0100

    lsp: move HTML lsp settings & capabilities to html.lua file

--- lua/lsp/servers/html.lua
+++ lua/lsp/servers/html.lua
@@ -1,7 +1,23 @@
---Enable (broadcasting) snippet capability for completion
+local M = {}
+
+-- Auto-install
+
+local lsp_installer_servers = require'nvim-lsp-installer.servers'
+
+local ok, html = lsp_installer_servers.get_server("html")
+if ok then
+    if not html:is_installed() then
+        html:install()
+    end
+end
+
+-- Settings
+
+-- Enable (broadcasting) snippet capability for completion
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-require'lspconfig'.html.setup {
-  capabilities = capabilities,
-}
+M.capabilities = capabilities
+M.settings = {}
+
+return M
