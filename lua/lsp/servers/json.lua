559d2fe190327a037e1e2e8f85cf773bc46687a4
ecosse3 <luk.kurpiewski@gmail.com>
2021-12-01 00:53:31 +0100

    lsp: add awesome frontend schemas for JSON via LSP

--- lua/lsp/servers/json.lua
+++ lua/lsp/servers/json.lua
@@ -13,6 +13,39 @@ end
 
 -- Settings
 
-M.settings = {}
+M.settings = {
+  json = {
+    schemas = {
+      {
+        fileMatch = {"package.json"},
+        url = "https://json.schemastore.org/package.json"
+      },
+      {
+        fileMatch = {"tsconfig*.json"},
+        url = "https://json.schemastore.org/tsconfig.json"
+      },
+      {
+        fileMatch = {".prettierrc", ".prettierrc.json", "prettier.config.json"},
+        url = "https://json.schemastore.org/prettierrc.json"
+      },
+      {
+        fileMatch = {".eslintrc", ".eslintrc.json"},
+        url = "https://json.schemastore.org/eslintrc.json"
+      },
+      {
+        fileMatch = {".babelrc", ".babelrc.json", "babel.config.json"},
+        url = "https://json.schemastore.org/babelrc.json"
+      },
+      {
+        fileMatch = {"lerna.json"},
+        url = "https://json.schemastore.org/lerna.json"
+      },
+      {
+        fileMatch = {"now.json", "vercel.json"},
+        url = "https://json.schemastore.org/now.json"
+      },
+    }
+  }
+}
 
 return M
