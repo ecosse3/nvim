403618eb09bad226d8b75dc3775de99389abe889
ecosse3 <luk.kurpiewski@gmail.com>
2021-12-01 00:52:56 +0100

    functions: add awesome folding function via treesitter

--- lua/functions.lua
+++ lua/functions.lua
@@ -1,9 +1,20 @@
--- Custom Folds
+local utils = require('utils')
+local async = require("plenary.async")
+
+-- Custom Folds, make them look better
 vim.cmd([[
   function! CustomFold()
     return printf('   %-6d%s', v:foldend - v:foldstart + 1, getline(v:foldstart))
   endfunction
 ]])
 
+-- It manages folds automatically based on treesitter
+local parsers = require'nvim-treesitter.parsers'
+local configs = parsers.get_parser_configs()
+local ft_str = table.concat(vim.tbl_map(function(ft) return configs[ft].filetype or ft end, parsers.available_parsers()), ',')
+print()
+vim.cmd('autocmd Filetype ' .. ft_str .. ' setlocal foldmethod=expr foldexpr=nvim_treesitter#foldexpr()')
+
+-- Exported functions
 local M = {}
 return M
