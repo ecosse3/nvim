9b825db09b740862331daa4a62495488720365e5
ecosse3 <luk.kurpiewski@gmail.com>
2021-12-02 02:02:45 +0100

    settings: update settings.lua and make it more better to maintain with vim.opt

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,59 +1,65 @@
-vim.o.encoding        = "utf-8"                     --- The encoding displayed
-vim.o.fileencoding    = "utf-8"                     --- The encoding written to file
-vim.o.cmdheight       = 2                           --- Give more space for displaying messages
-vim.o.splitright      = true                        --- Vertical splits will automatically be to the right
-vim.o.updatetime      = 100                         --- Faster completion
-vim.o.timeoutlen      = 300                         --- Faster completion
-vim.o.clipboard       = "unnamed,unnamedplus"       --- Copy-paste between vim and everything else
-vim.o.mouse           = "a"                         --- Enable mouse
-vim.o.ignorecase      = true                        --- Needed for smartcase
-vim.o.smartcase       = true                        --- Uses case in search
-vim.o.smarttab        = true                        --- Makes tabbing smarter will realize you have 2 vs 4
-vim.bo.smartindent    = true                        --- Makes indenting smart
-vim.bo.shiftwidth     = 2                           --- Change a number of space characeters inseted for indentation
-vim.o.shiftwidth      = 2                           --- Change a number of space characeters inseted for indentation
-vim.o.showtabline     = 2                           --- Always show tabs
-vim.o.tabstop         = 2                           --- Insert 2 spaces for a tab
-vim.bo.tabstop        = 2                           --- Insert 2 spaces for a tab
-vim.o.softtabstop     = 2                           --- Insert 2 spaces for a tab
-vim.bo.softtabstop    = 2                           --- Insert 2 spaces for a tab
-vim.o.showmode        = false                       --- Don't show things like -- INSERT -- anymore
-vim.o.autoindent      = true                        --- Good auto indent
-vim.o.errorbells      = false                       --- Disables sound effect for errors
-vim.wo.number         = true                        --- Shows current line number
-vim.wo.relativenumber = true                        --- Enables relative number
-vim.wo.cursorline     = true                        --- Highlight of current line
-vim.wo.wrap           = false                       --- Display long lines as just one line
-vim.o.backup          = false                       --- Recommended by coc
-vim.o.writebackup     = false                       --- Recommended by coc
-vim.o.swapfile        = false                       --- Recommended by coc
-vim.o.emoji           = false                       --- Fix emoji display
-vim.o.undodir         = os.getenv("HOME") .. "/.config/nvim/undodir" --- Dir for undos
-vim.o.undofile        = true                        --- Sets undo to file
-vim.o.incsearch       = true                        --- Start searching before pressing enter
-vim.o.conceallevel    = 0                           --- Show `` in markdown files
-vim.o.backspace       = "indent,eol,start"          --- Making sure backspace works
-vim.o.lazyredraw      = true                        --- Makes macros faster & prevent errors in complicated mappings
-vim.o.wildignore      = "*node_modules/**"          --- Don't search inside Node.js modules (works for gutentag)
-vim.o.scrolloff       = 8                           --- Always keep space when scrolling to bottom/top edge
-vim.o.viminfo         = "'1000"                     --- Increase the size of file history
-vim.o.termguicolors   = true                        --- Correct terminal colors
-vim.o.signcolumn      = "yes"                       --- Add extra sign column next to line number
-vim.o.completeopt     = "menu,menuone,noselect"     --- Better autocompletion
-vim.o.foldtext        = "CustomFold()"              --- Emit custom function for foldtext
-vim.g.mapleader       = " "                         --- Map leader key
-vim.o.shortmess       = vim.o.shortmess .. 'c'      --- Don't pass messages to |ins-completion-menu|
-vim.o.expandtab       = true
-vim.bo.expandtab      = true
-vim.o.foldlevelstart  = 99                          --- Expand all folds by default
+local options = {
+  clipboard      = "unnamed,unnamedplus",   --- Copy-paste between vim and everything else
+  cmdheight      = 2,                       --- Give more space for displaying messages
+  completeopt    = "menu,menuone,noselect", --- Better autocompletion
+  cursorline     = true,                    --- Highlight of current line
+  emoji          = false,                   --- Fix emoji display
+  expandtab      = true,                    --- Use spaces instead of tabs
+  foldlevelstart = 99,                      --- Expand all folds by default
+  foldtext       = "CustomFold()",          --- Emit custom function for foldtext
+  ignorecase     = true,                    --- Needed for smartcase
+  lazyredraw     = true,                    --- Makes macros faster & prevent errors in complicated mappings
+  mouse          = "a",                     --- Enable mouse
+  number         = true,                    --- Shows current line number
+  relativenumber = true,                    --- Enables relative number
+  scrolloff      = 8,                       --- Always keep space when scrolling to bottom/top edge
+  shiftwidth     = 2,                       --- Change a number of space characeters inseted for indentation
+  showtabline    = 2,                       --- Always show tabs
+  signcolumn     = "yes",                   --- Add extra sign column next to line number
+  smartcase      = true,                    --- Uses case in search
+  smartindent    = true,                    --- Makes indenting smart
+  smarttab       = true,                    --- Makes tabbing smarter will realize you have 2 vs 4
+  softtabstop    = 2,                       --- Insert 2 spaces for a tab
+  splitright     = true,                    --- Vertical splits will automatically be to the right
+  swapfile       = false,                   --- Swap not needed
+  tabstop        = 2,                       --- Insert 2 spaces for a tab
+  termguicolors  = true,                    --- Correct terminal colors
+  timeoutlen     = 300,                     --- Faster completion
+  undofile       = true,                    --- Sets undo to file
+  updatetime     = 100,                     --- Faster completion
+  viminfo        = "'1000",                 --- Increase the size of file history
+  wildignore     = "*node_modules/**",      --- Don't search inside Node.js modules (works for gutentag)
+  wrap           = false,                   --- Display long lines as just one line
+  writebackup    = false,                   --- Not needed
+  -- Neovim defaults
+  autoindent     = true,                    --- Good auto indent
+  backspace      = "indent,eol,start",      --- Making sure backspace works
+  backup         = false,                   --- Recommended by coc
+  conceallevel   = 0,                       --- Show `` in markdown files
+  encoding       = "utf-8",                 --- The encoding displayed
+  errorbells     = false,                   --- Disables sound effect for errors
+  fileencoding   = "utf-8",                 --- The encoding written to file
+  incsearch      = true,                    --- Start searching before pressing enter
+  showmode       = false,                   --- Don't show things like -- INSERT -- anymore
+}
 
-vim.g.did_load_filetyps           = 1               --- Do not source the default filetype.vim
-vim.g.speeddating_no_mappings     = 1               --- Disable default mappings for speeddating
-vim.g.dashboard_default_executive = EcoVim.plugins.dashboard.fuzzy_plugin --- Use fuzzy search plugin in dashboard
+local globals = {
+  dashboard_default_executive = EcoVim.plugins.dashboard.fuzzy_plugin, --- Use fuzzy search plugin in dashboard
+  did_load_filetyps           = 1,          --- Do not source the default filetype.vim
+  fillchars                   = "fold:\\ ", --- Fill chars needed for folds
+  mapleader                   = ' ',        --- Map leader key to SPC
+  speeddating_no_mappings     = 1,          --- Disable default mappings for speeddating
+}
 
-vim.cmd('filetype plugin indent on')                --- Enables plugin & indent
-vim.cmd('set formatoptions-=c')                     --- Stop newline continuation of comments
-vim.cmd('set formatoptions-=r')
-vim.cmd('set formatoptions-=o')
-vim.cmd('set completeopt=menu,menuone,noselect')
-vim.cmd('set fillchars=fold:\\ ')
+vim.opt.shortmess:append('c');
+vim.opt.formatoptions:remove('c');
+vim.opt.formatoptions:remove('r');
+vim.opt.formatoptions:remove('o');
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+
+for k, v in pairs(globals) do
+  vim.g[k] = v
+end
