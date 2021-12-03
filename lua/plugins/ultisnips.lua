0dcab04d8fedb9223042dce7f2251251db0ff95c
ecosse3 <luk.kurpiewski@gmail.com>
2021-12-03 23:31:11 +0100

    Remove unnecessary require of ultisnips settings

--- lua/plugins/ultisnips.lua
+++ lua/plugins/ultisnips.lua
@@ -1,5 +1,3 @@
-require("cmp-nvim-ultisnips").setup {}
-
 vim.g.UltiSnipsJumpForwardTrigger       = '<C-]>'
 vim.g.UltiSnipsJumpBackwardTrigger      = '<C-[>'
 vim.g.UltiSnipsRemoveSelectModeMappings = 0
