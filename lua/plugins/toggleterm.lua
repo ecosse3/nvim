d8ca14be9fc14e751c3f3b59070e32e2cb787e74
ecosse3 <luk.kurpiewski@gmail.com>
2021-12-04 00:37:11 +0100

    keymappings: allow to go to NORMAL Mode in floating terminal to close it with same keybinding (<leader>at)

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -47,3 +47,15 @@ function Lazygit_toggle()
 end
 
 vim.api.nvim_set_keymap("n", "<Space>gg", "<cmd>lua Lazygit_toggle()<CR>", {noremap = true, silent = true})
+
+function _G.set_terminal_keymaps()
+  local opts = {noremap = true}
+  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+end
+
+vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
