12473417fef2526a9230d55a20dbdc98f91e09ad
ecosse3 <luk.kurpiewski@gmail.com>
2021-12-19 23:55:54 +0100

    lsp: add border from EcoVim global to textDocument/hover & signatureHelp lsp handlers

--- lua/lsp/config.lua
+++ lua/lsp/config.lua
@@ -1,3 +1,7 @@
+local M = {}
+
+-- UI
+
 local signs = { Error = " 🞮", Warn = " ▲", Hint = " ", Info = " " }
 
 for type, icon in pairs(signs) do
@@ -5,6 +9,11 @@ for type, icon in pairs(signs) do
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
+-- LSP settings (for overriding per client)
+M.handlers =  {
+  ["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = EcoVim.ui.float.border}),
+  ["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = EcoVim.ui.float.border}),
+}
 -- Load servers (They will be automatically installed after next "Sync plugins" launch)
 -- Check installed servers by :LspInstallInfo
 
@@ -17,3 +26,5 @@ end
 -- require('lsp.servers.lua')
 -- require('lsp.servers.tailwind')
 -- require('lsp.servers.tsserver')
+
+return M
