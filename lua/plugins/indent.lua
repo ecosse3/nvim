a3ba280c5b6e06802d637691411e74821f271da3
ecosse3 <luk.kurpiewski@gmail.com>
2021-12-20 21:15:13 +0100

    plugins: exclude indent for Float & nofile types

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -5,8 +5,8 @@ vim.opt.listchars = {
 
 require("indent_blankline").setup {
   char = "|",
-  buftype_exclude = {"terminal", "telescope"},
-  filetype_exclude = {"help", "dashboard", "packer", "NvimTree", "Trouble", "TelescopePrompt"},
+  buftype_exclude = {"terminal", "telescope", "nofile"},
+  filetype_exclude = {"help", "dashboard", "packer", "NvimTree", "Trouble", "TelescopePrompt", "Float"},
   space_char_blankline = " ",
   show_current_context = true,
   show_end_of_line = true,
