3549c179786833b390fa8de852753a1ca9d2c3e2
ecosse3 <luk.kurpiewski@gmail.com>
2021-12-20 21:03:01 +0100

    plugins: ignore TelescopePrompt in indent blankline

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -6,7 +6,7 @@ vim.opt.listchars = {
 require("indent_blankline").setup {
   char = "|",
   buftype_exclude = {"terminal", "telescope"},
-  filetype_exclude = {"help", "dashboard", "packer", "NvimTree", "Trouble"},
+  filetype_exclude = {"help", "dashboard", "packer", "NvimTree", "Trouble", "TelescopePrompt"},
   space_char_blankline = " ",
   show_current_context = true,
   show_end_of_line = true,
