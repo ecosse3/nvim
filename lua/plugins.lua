a0ded81d7a87c00a6cc6494b17ff55e0d66bd623
ecosse3 <luk.kurpiewski@gmail.com>
2021-12-22 00:23:41 +0100

    Remove ultisnips

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -73,7 +73,6 @@ local function init()
   use {'hrsh7th/cmp-calc', after = 'cmp-path'}
   use {'tzachar/cmp-tabnine', run = './install.sh', requires = 'hrsh7th/nvim-cmp', after = 'cmp-calc'}
   use {'David-Kunz/cmp-npm', after = 'cmp-tabnine', requires = 'nvim-lua/plenary.nvim', config = "require('plugins.cmp-npm')"}
-  use {'quangnguyen30192/cmp-nvim-ultisnips', after = 'cmp-npm'}
 
   -- LSP Addons
   use {'williamboman/nvim-lsp-installer', event = 'BufEnter', after = 'cmp-nvim-lsp', config = "require('lsp.installer')"}
@@ -116,7 +115,6 @@ local function init()
   use {'potatoesmaster/i3-vim-syntax'}
   use {'lukas-reineke/indent-blankline.nvim', config = "require('plugins.indent')"}
   use {'norcalli/nvim-colorizer.lua', config = "require('plugins.colorizer')"}
-  use {'SirVer/ultisnips', requires = {"honza/vim-snippets"}, config = "require('plugins.ultisnips')", after = 'cmp-nvim-ultisnips'}
   use {'lpinilla/vim-codepainter'}
 
   -- Nvim Tree
--- lua/plugins/ultisnips.lua
+++
@@ -1,3 +0,0 @@
-vim.g.UltiSnipsJumpForwardTrigger       = '<C-]>'
-vim.g.UltiSnipsJumpBackwardTrigger      = '<C-[>'
-vim.g.UltiSnipsRemoveSelectModeMappings = 0
