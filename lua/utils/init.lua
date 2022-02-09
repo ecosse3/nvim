6a6a341c2eae276814838124eb30a20aee80eb77
ecosse3 <luk.kurpiewski@gmail.com>
2022-02-09 21:45:27 +0100

    feat(utils): refactor utils to utils/init and add couple new functions

---
+++ lua/utils/init.lua
@@ -0,0 +1,112 @@
+local M = {}
+local fn = vim.fn
+
+M.file_exists = function(path)
+  local f=io.open(path, "r")
+   if f~=nil then io.close(f) return true else return false end
+end
+
+M.get_relative_fname = function()
+  local fname = vim.fn.expand('%:p')
+  return fname:gsub(vim.fn.getcwd() .. '/', '')
+end
+
+M.get_relative_gitdir = function()
+  local fname = vim.fn.expand('%:p')
+  local gitpath = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+  return fname:gsub(gitpath .. '/', '')
+end
+
+M.sleep = function(n)
+  os.execute("sleep " .. tonumber(n))
+end
+
+M.toggle_quicklist = function()
+  if fn.empty(fn.filter(fn.getwininfo(), 'v:val.quickfix')) == 1 then
+    vim.cmd('copen')
+  else
+    vim.cmd('cclose')
+  end
+end
+
+M.starts_with = function(str, start)
+  return str:sub(1, #start) == start
+end
+
+M.end_with = function(str, ending)
+  return ending == "" or str:sub(-#ending) == ending
+end
+
+M.split = function(s, delimiter)
+  local result = {}
+  for match in (s .. delimiter):gmatch("(.-)" .. delimiter) do
+    table.insert(result, match)
+  end
+
+  return result
+end
+
+M.handle_job_data = function(data)
+  if not data then
+    return nil
+  end
+  if data[#data] == "" then
+    table.remove(data, #data)
+  end
+  if #data < 1 then
+    return nil
+  end
+  return data
+end
+
+M.warnlog = function(message, title)
+  require('notify')(message, "warn", { title = title or "Warning" })
+end
+
+M.errorlog = function(message, title)
+  require('notify')(message, "error", { title = title or "Error" })
+end
+
+M.jobstart = function(cmd, on_finish)
+  local has_error = false
+  local lines = {}
+
+  local function on_event(_, data, event)
+    if event == "stdout" then
+      data = M.handle_job_data(data)
+      if not data then
+        return
+      end
+
+      for i = 1, #data do
+        table.insert(lines, data[i])
+      end
+    elseif event == "stderr" then
+      data = M.handle_job_data(data)
+      if not data then
+        return
+      end
+
+      has_error = true
+      local error_message = ""
+      for _, line in ipairs(data) do
+        error_message = error_message .. line
+      end
+      M.log("Error during running a job: " .. error_message)
+    elseif event == "exit" then
+      if not has_error then
+        on_finish(lines)
+      end
+    end
+  end
+
+  vim.fn.jobstart(cmd, {
+    on_stderr = on_event,
+    on_stdout = on_event,
+    on_exit = on_event,
+    stdout_buffered = true,
+    stderr_buffered = true,
+  })
+end
+
+return M
