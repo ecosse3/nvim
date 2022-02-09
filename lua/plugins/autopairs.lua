9d98b7cbc8e3f76a34ffa069386c43f6bb7a9c9c
ecosse3 <luk.kurpiewski@gmail.com>
2022-02-09 21:46:09 +0100

    feat(plugins): enhancement in autopairs plugin config

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,1 +1,5 @@
 require('nvim-autopairs').setup()
+
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
