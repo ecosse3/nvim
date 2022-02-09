5c15c11bcb62ae04d7d5533b3937a2d30072c62f
ecosse3 <luk.kurpiewski@gmail.com>
2022-02-09 21:45:46 +0100

    feat(utils): add globals functions to debug

---
+++ lua/globals.lua
@@ -0,0 +1,13 @@
+P = function(v)
+  print(vim.inspect(v))
+  return v
+end
+
+RELOAD = function(...)
+  return require("plenary.reload").reload_module(...)
+end
+
+R = function(name)
+  RELOAD(name)
+  return require(name)
+end
