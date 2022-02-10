f79d6678ec8c5a775d398f78c3bd474556fa2a7b
ecosse3 <luk.kurpiewski@gmail.com>
2022-02-11 00:25:45 +0100

    feat(tree): enable update_cwd option in Nvim Tree

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -16,7 +16,7 @@ require'nvim-tree'.setup {
   -- hijack the cursor in the tree to put it at the start of the filename
   hijack_cursor       = false,
   -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
-  update_cwd          = false,
+  update_cwd          = true,
   -- show lsp diagnostics in the signcolumn
   diagnostics         = {
     enable = false,
