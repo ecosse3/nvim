035c9b4e4e70da6651a6017ed9318ccf87edc00e
ecosse3 <luk.kurpiewski@gmail.com>
2022-03-05 00:14:52 +0100

    fix(plugins): remove default and deprecated config from dressing.lua

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -1,46 +1,7 @@
 require('dressing').setup({
-  input = {
-    -- Default prompt string
-    default_prompt = "➤ ",
-
-    -- When true, <Esc> will close the modal
-    insert_only = true,
-
-    -- These are passed to nvim_open_win
-    anchor = "SW",
-    relative = "cursor",
-    row = 0,
-    col = 0,
-    border = "rounded",
-
-    -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-    prefer_width = 40,
-    max_width = nil,
-    min_width = 20,
-
-    -- Window transparency (0-100)
-    winblend = 10,
-
-    -- see :help dressing_get_config
-    get_config = nil,
-  },
   select = {
     -- Priority list of preferred vim.select implementations
     backend = { "telescope", "nui", "fzf", "builtin" },
-
-    -- Options for telescope selector
-    telescope = {
-      -- can be 'dropdown', 'cursor', or 'ivy'
-      theme = "dropdown",
-    },
-
-    -- Options for fzf selector
-    fzf = {
-      window = {
-        width = 0.5,
-        height = 0.4,
-      },
-    },
 
     -- Options for nui Menu
     nui = {
@@ -60,30 +21,6 @@ require('dressing').setup({
       max_width = 80,
       max_height = 40,
     },
-
-    -- Options for built-in selector
-    builtin = {
-      -- These are passed to nvim_open_win
-      anchor = "NW",
-      relative = "cursor",
-      row = 0,
-      col = 0,
-      border = "rounded",
-
-      -- Window transparency (0-100)
-      winblend = 10,
-
-      -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-      width = nil,
-      max_width = 0.8,
-      min_width = 40,
-      height = nil,
-      max_height = 0.9,
-      min_height = 10,
-    },
-
-    -- Used to override format_item. See :help dressing-format
-    format_item_override = {},
 
     -- see :help dressing_get_config
     get_config = function(opts)
