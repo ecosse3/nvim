85ec23395b625301f86d22cfa0d1be0aad9843ac
ecosse3 <luk.kurpiewski@gmail.com>
2022-04-22 00:17:55 +0200

    feat(autopairs): use treesitter for auto-pairing

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,4 +1,11 @@
-require('nvim-autopairs').setup()
+require('nvim-autopairs').setup({
+  check_ts = true,
+  ts_config = {
+    lua = { 'string' },
+    javascript = { 'template_string' },
+    java = false,
+  }
+})
 
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
