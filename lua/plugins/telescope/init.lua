cb085e6336004e5b7904cdd1781a4c14a892e143
ecosse3 <luk.kurpiewski@gmail.com>
2022-04-22 17:32:05 +0200

    style(plugins): lint telescope

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -1,63 +1,65 @@
 local actions    = require('telescope.actions')
 local previewers = require('telescope.previewers')
 local builtin    = require('telescope.builtin')
-require('telescope').setup {
-    defaults = {
-        vimgrep_arguments = {
-          'rg',
-          '--color=never',
-          '--no-heading',
-          '--with-filename',
-          '--line-number',
-          '--column',
-          '--smart-case'
-        },
-        layout_config = {
-          horizontal = {
-            mirror = false,
-          },
-          vertical = {
-            mirror = false,
-          },
-          prompt_position = "top",
-        },
-        file_sorter      = require('telescope.sorters').get_fzy_sorter,
-        prompt_prefix    = ' 🔍 ',
-        color_devicons   = true,
-
-        sorting_strategy = "ascending",
-
-        file_previewer   = require('telescope.previewers').vim_buffer_cat.new,
-        grep_previewer   = require('telescope.previewers').vim_buffer_vimgrep.new,
-        qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
-
-        mappings = {
-            i = {
-                ["<C-x>"] = false,
-                ["<C-j>"] = actions.move_selection_next,
-                ["<C-k>"] = actions.move_selection_previous,
-                ["<C-q>"] = actions.send_selected_to_qflist,
-                ["<C-s>"] = actions.cycle_previewers_next,
-                ["<C-a>"] = actions.cycle_previewers_prev,
-            },
-            n = {
-                ["<C-s>"] = actions.cycle_previewers_next,
-                ["<C-a>"] = actions.cycle_previewers_prev,
-            }
-        }
-    },
-    extensions = {
-        fzf = {
-            override_generic_sorter = false,
-            override_file_sorter = true,
-            case_mode = "smart_case",
-        }
-    }
-}
 
 require('telescope').load_extension('fzf')
 require('telescope').load_extension('repo')
 
+require('telescope').setup {
+  defaults = {
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case'
+    },
+    layout_config     = {
+      horizontal = {
+        width = 0.9,
+        height = 0.95,
+        preview_cutoff = 120,
+      },
+      vertical = {
+        mirror = false,
+      },
+      prompt_position = "top",
+    },
+    file_sorter       = require('telescope.sorters').get_fzy_sorter,
+    prompt_prefix     = ' 🔍 ',
+    color_devicons    = true,
+
+    sorting_strategy = "ascending",
+
+    file_previewer   = require('telescope.previewers').vim_buffer_cat.new,
+    grep_previewer   = require('telescope.previewers').vim_buffer_vimgrep.new,
+    qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+
+    mappings = {
+      i = {
+        ["<C-x>"] = false,
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<C-q>"] = actions.send_selected_to_qflist,
+        ["<C-s>"] = actions.cycle_previewers_next,
+        ["<C-a>"] = actions.cycle_previewers_prev,
+      },
+      n = {
+        ["<C-s>"] = actions.cycle_previewers_next,
+        ["<C-a>"] = actions.cycle_previewers_prev,
+      }
+    }
+  },
+  extensions = {
+    fzf = {
+      override_generic_sorter = false,
+      override_file_sorter = true,
+      case_mode = "smart_case",
+    }
+  }
+}
 
 -- Implement delta as previewer for diffs
 
@@ -99,11 +101,11 @@ end
 
 M.edit_neovim = function()
   builtin.git_files {
-    cwd = "~/.config/nvim",
-    prompt = "~ dotfiles ~",
+    cwd              = "~/.config/nvim",
+    prompt           = "~ dotfiles ~",
     color_devicons   = true,
     sorting_strategy = "ascending",
-    layout_config = {
+    layout_config    = {
       horizontal = {
         mirror = false,
       },
@@ -117,8 +119,8 @@ end
 
 M.project_files = function()
   local opts = {} -- define here if you want to define something
-  local ok = pcall(require"telescope.builtin".git_files, opts)
-  if not ok then require"telescope.builtin".find_files(opts) end
+  local ok = pcall(require "telescope.builtin".git_files, opts)
+  if not ok then require "telescope.builtin".find_files(opts) end
 end
 
 return M
