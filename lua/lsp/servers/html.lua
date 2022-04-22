cc784460908e9e3725c222f60806ac96e4b807ee
ecosse3 <luk.kurpiewski@gmail.com>
2022-04-22 17:26:39 +0200

    fix(lsp): make sure that cmp_nvim_lsp is loaded before updating capabilities for html

--- lua/lsp/servers/html.lua
+++ lua/lsp/servers/html.lua
@@ -15,8 +15,12 @@ end
 -- Settings
 
 -- Enable (broadcasting) snippet capability for completion
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-capabilities.textDocument.completion.completionItem.snippetSupport = true
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+local loaded_cmp_nvim_lsp, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
+if loaded_cmp_nvim_lsp then
+  capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities.textDocument.completion.completionItem.snippetSupport = true
+end
 
 M.capabilities = capabilities
 M.settings = {}
