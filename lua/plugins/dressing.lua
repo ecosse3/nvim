bea48980151790f36e7d600fee3a0c60a05ef131
ecosse3 <luk.kurpiewski@gmail.com>
2022-04-23 20:31:23 +0200

    chore(plugins): update dressing config

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -1,4 +1,46 @@
 require('dressing').setup({
+  input = {
+    -- Set to false to disable the vim.ui.input implementation
+    enabled = true,
+
+    -- Default prompt string
+    default_prompt = "Input:",
+
+    -- Can be 'left', 'right', or 'center'
+    prompt_align = "left",
+
+    -- When true, <Esc> will close the modal
+    insert_only = true,
+
+    -- These are passed to nvim_open_win
+    anchor = "SW",
+    border = EcoVim.ui.float.border or "rounded",
+    -- 'editor' and 'win' will default to being centered
+    relative = "cursor",
+
+    -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+    prefer_width = 40,
+    width = nil,
+    -- min_width and max_width can be a list of mixed types.
+    -- min_width = {20, 0.2} means "the greater of 20 columns or 20% of total"
+    max_width = { 140, 0.9 },
+    min_width = { 20, 0.2 },
+
+    -- Window transparency (0-100)
+    winblend = 0,
+    -- Change default highlight groups (see :help winhl)
+    winhighlight = "",
+
+    override = function(conf)
+      -- This is the config that will be passed to nvim_open_win.
+      -- Change values here to customize the layout
+      return conf
+    end,
+
+    -- see :help dressing_get_config
+    get_config = nil,
+  },
+
   select = {
     -- Priority list of preferred vim.select implementations
     backend = { "telescope", "nui", "fzf", "builtin" },
@@ -12,8 +54,7 @@ require('dressing').setup({
       size = nil,
       relative = "cursor",
       border = {
-        style = "rounded",
-        highlight = "NightflyRed",
+        style = EcoVim.ui.float.border or "rounded",
         text = {
           top_align = "right",
         },
@@ -21,6 +62,7 @@ require('dressing').setup({
       max_width = 80,
       max_height = 40,
     },
+
 
     -- see :help dressing_get_config
     get_config = function(opts)
