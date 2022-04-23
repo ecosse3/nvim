274a2d5194997916c88f04ff5a4dd39a42b6cc1b
ecosse3 <luk.kurpiewski@gmail.com>
2022-04-24 00:26:06 +0200

    feat(autocmds): enable spell checking for certain file types (txt, md, tex)

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -5,3 +5,5 @@ vim.api.nvim_create_autocmd("TextYankPost", { callback = function() vim.highligh
 -- Disable diagnostics in node_modules (0 is current buffer only)
 vim.api.nvim_create_autocmd("BufRead", { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" })
 vim.api.nvim_create_autocmd("BufNewFile", { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" })
+-- Enable spell checking for certain file types
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, { pattern = { "*.txt", "*.md", "*.tex" }, command = "setlocal spell" })
