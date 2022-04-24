2d18979d30523f37fb7ea8f862757a080981eabe
ecosse3 <luk.kurpiewski@gmail.com>
2022-04-24 16:24:36 +0200

    refactor(keymappings): switch to new vim.keymap.set API

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,5 +1,4 @@
-local keymap = vim.api.nvim_set_keymap;
-local opts = { noremap = true, silent = true }
+local keymap = vim.keymap.set
 local silent = { silent = true }
 
 -- Better window movement
@@ -9,38 +8,38 @@ keymap("n", "<C-k>", "<C-w>k", silent)
 keymap("n", "<C-l>", "<C-w>l", silent)
 
 -- Move selected line / block of text in visual mode
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", silent)
+keymap("x", "J", ":move '>+1<CR>gv-gv", silent)
 
 -- Keep visual mode indenting
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+keymap("v", "<", "<gv", silent)
+keymap("v", ">", ">gv", silent)
 
 -- Save file by CTRL-S
-keymap("n", "<C-s>", ":w<CR>", opts)
-keymap("i", "<C-s>", "<ESC> :w<CR>", opts)
+keymap("n", "<C-s>", ":w<CR>", silent)
+keymap("i", "<C-s>", "<ESC> :w<CR>", silent)
 
 -- Telescope
-keymap("n", "<C-p>", "<CMD>lua require('plugins.telescope').project_files()<CR>", { noremap = true })
-keymap("n", "<S-p>", "<CMD>lua require('plugins.telescope.pickers.multi-rg')()<CR>", { noremap = true })
+keymap("n", "<C-p>", "<CMD>lua require('plugins.telescope').project_files()<CR>")
+keymap("n", "<S-p>", "<CMD>lua require('plugins.telescope.pickers.multi-rg')()<CR>")
 
 -- Remove highlights
-keymap("n", "<CR>", ":noh<CR><CR>", opts)
+keymap("n", "<CR>", ":noh<CR><CR>", silent)
 
 -- Find word/file across project
-keymap("n", "<Leader>pf", "yiw<CMD>Telescope git_files<CR><C-r>+<ESC>", { noremap = true })
-keymap("n", "<Leader>pw", "<CMD>Telescope grep_string<CR><ESC>", { noremap = true })
+keymap("n", "<Leader>pf", "yiw<CMD>Telescope git_files<CR><C-r>+<ESC>")
+keymap("n", "<Leader>pw", "<CMD>Telescope grep_string<CR><ESC>")
 
 -- Git
-keymap("n", "<Leader>gla", "<CMD>lua require('plugins.telescope').my_git_commits()<CR>", opts)
-keymap("n", "<Leader>glc", "<CMD>lua require('plugins.telescope').my_git_bcommits()<CR>", opts)
+keymap("n", "<Leader>gla", "<CMD>lua require('plugins.telescope').my_git_commits()<CR>", {})
+keymap("n", "<Leader>glc", "<CMD>lua require('plugins.telescope').my_git_bcommits()<CR>", silent)
 
 -- Buffers
-keymap("n", "<Tab>", ":BufferNext<CR>", opts)
-keymap("n", "gn", ":bn<CR>", opts)
-keymap("n", "<S-Tab>", ":BufferPrevious<CR>", opts)
-keymap("n", "gp", ":bp<CR>", opts)
-keymap("n", "<S-q>", ":BufferClose<CR>", opts)
+keymap("n", "<Tab>", ":BufferNext<CR>", silent)
+keymap("n", "gn", ":bn<CR>", silent)
+keymap("n", "<S-Tab>", ":BufferPrevious<CR>", silent)
+keymap("n", "gp", ":bp<CR>", silent)
+keymap("n", "<S-q>", ":BufferClose<CR>", silent)
 
 -- Move between barbar buffers
 keymap("n", "<Space>1", ":BufferGoto 1<CR>", silent)
@@ -63,13 +62,13 @@ keymap("n", "<A-8>", ":BufferGoto 8<CR>", silent)
 keymap("n", "<A-9>", ":BufferGoto 9<CR>", silent)
 
 -- Don't yank on delete char
-keymap("n", "x", '"_x', opts)
-keymap("n", "X", '"_X', opts)
-keymap("v", "x", '"_x', opts)
-keymap("v", "X", '"_X', opts)
+keymap("n", "x", '"_x', silent)
+keymap("n", "X", '"_X', silent)
+keymap("v", "x", '"_x', silent)
+keymap("v", "X", '"_X', silent)
 
 -- Don't yank on visual paste
-keymap("v", "p", '"_dP', opts)
+keymap("v", "p", '"_dP', silent)
 
 -- Avoid issues because of remapping <c-a> and <c-x> below
 vim.cmd [[
@@ -82,15 +81,15 @@ keymap("n", "<Space>,", ":cp<CR>", silent)
 keymap("n", "<Space>.", ":cn<CR>", silent)
 
 -- Toggle quicklist
-keymap("n", "<leader>q", "<cmd>lua require('utils').toggle_quicklist()<CR>", { noremap = true, silent = true})
+keymap("n", "<leader>q", "<cmd>lua require('utils').toggle_quicklist()<CR>", silent)
 
 -- Easyalign
 keymap("n", "ga", "<Plug>(EasyAlign)", silent)
 keymap("x", "ga", "<Plug>(EasyAlign)", silent)
 
 -- Manually invoke speeddating in case switch.vim didn't work
-keymap("n", "<C-a>", ":if !switch#Switch() <bar> call speeddating#increment(v:count1) <bar> endif<CR>", opts)
-keymap("n", "<C-x>", ":if !switch#Switch({'reverse': 1}) <bar> call speeddating#increment(-v:count1) <bar> endif<CR>", opts)
+keymap("n", "<C-a>", ":if !switch#Switch() <bar> call speeddating#increment(v:count1) <bar> endif<CR>", silent)
+keymap("n", "<C-x>", ":if !switch#Switch({'reverse': 1}) <bar> call speeddating#increment(-v:count1) <bar> endif<CR>", silent)
 
 -- Open links under cursor in browser with gx
 if vim.fn.has('macunix') == 1 then
@@ -100,15 +99,15 @@ else
 end
 
 -- LSP
-keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", silent)
+keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", silent)
 keymap("n", "<C-Space>", "<cmd>lua vim.lsp.buf.code_action()<CR>", silent)
-keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-keymap("v", "<leader>ca", "<cmd>'<,'>lua vim.lsp.buf.range_code_action()<CR>", opts)
-keymap("n", "<leader>cr", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-keymap("n", "<leader>cf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-keymap("v", "<leader>cf", "<cmd>'<.'>lua vim.lsp.buf.range_formatting()<CR>", opts)
-keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-keymap("n", "L", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-keymap("n", "]g", "<cmd>lua vim.diagnostic.goto_next({ float = { border = 'rounded' }})<CR>", opts)
-keymap("n", "[g", "<cmd>lua vim.diagnostic.goto_prev({ float = { border = 'rounded' }})<CR>", opts)
+keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", silent)
+keymap("v", "<leader>ca", "<cmd>'<,'>lua vim.lsp.buf.range_code_action()<CR>", silent)
+keymap("n", "<leader>cr", "<cmd>lua vim.lsp.buf.rename()<CR>", silent)
+keymap("n", "<leader>cf", "<cmd>lua vim.lsp.buf.formatting()<CR>", silent)
+keymap("v", "<leader>cf", "<cmd>'<.'>lua vim.lsp.buf.range_formatting()<CR>", silent)
+keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", silent)
+keymap("n", "L", "<cmd>lua vim.lsp.buf.signature_help()<CR>", silent)
+keymap("n", "]g", "<cmd>lua vim.diagnostic.goto_next({ float = { border = 'rounded' }})<CR>", silent)
+keymap("n", "[g", "<cmd>lua vim.diagnostic.goto_prev({ float = { border = 'rounded' }})<CR>", silent)
