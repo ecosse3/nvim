39065722830ea810be094c11d92c8317112e30b5
ecosse3 <luk.kurpiewski@gmail.com>
2022-04-28 16:14:00 +0200

    chore(lsp): add custom diagnostic codes for lua & tsserver

--- lua/lsp/config.lua
+++ lua/lsp/config.lua
@@ -1,14 +1,102 @@
 -- Diagnostic config
 
+local codes = {
+  -- Lua
+  no_matching_function = {
+    message = " Can't find a matching function",
+    "redundant-parameter",
+    "ovl_no_viable_function_in_call",
+  },
+  empty_block = {
+    message = " That shouldn't be empty here",
+    "empty-block",
+  },
+  missing_symbol = {
+    message = " Here should be a symbol",
+    "miss-symbol",
+  },
+  expected_semi_colon = {
+    message = " Please put the `;` or `,`",
+    "expected_semi_declaration",
+    "miss-sep-in-table",
+    "invalid_token_after_toplevel_declarator",
+  },
+  redefinition = {
+    message = " That variable was defined before",
+    icon = " ",
+    "redefinition",
+    "redefined-local",
+    "no-duplicate-imports",
+    "@typescript-eslint/no-redeclare",
+    "import/no-duplicates"
+  },
+  no_matching_variable = {
+    message = " Can't find that variable",
+    "undefined-global",
+    "reportUndefinedVariable",
+  },
+  trailing_whitespace = {
+    message = "  Whitespaces are useless",
+    "trailing-whitespace",
+    "trailing-space",
+  },
+  unused_variable = {
+    message = "  Don't define variables you don't use",
+    icon = "  ",
+    "unused-local",
+    "@typescript-eslint/no-unused-vars"
+  },
+  unused_function = {
+    message = "  Don't define functions you don't use",
+    "unused-function",
+  },
+  useless_symbols = {
+    message = " Remove that useless symbols",
+    "unknown-symbol",
+  },
+  wrong_type = {
+    message = " Try to use the correct types",
+    "init_conversion_failed",
+  },
+  undeclared_variable = {
+    message = " Have you delcared that variable somewhere?",
+    "undeclared_var_use",
+  },
+  lowercase_global = {
+    message = " Should that be a global? (if so make it uppercase)",
+    "lowercase-global",
+  },
+  -- Prettier
+  prettier = {
+    icon = "  ",
+    "prettier/prettier"
+  }
+}
+
 vim.diagnostic.config({
   float = {
+    source = false,
     format = function(diagnostic)
-      if not diagnostic.source or not diagnostic.user_data.lsp.code then
+      local code = diagnostic.user_data.lsp.code
+
+      if not diagnostic.source or not code then
         return string.format('%s', diagnostic.message)
       end
 
       if diagnostic.source == 'eslint' then
-        return string.format('%s [%s]', diagnostic.message, diagnostic.user_data.lsp.code)
+        for _, table in pairs(codes) do
+          if vim.tbl_contains(table, code) then
+            return string.format('%s [%s]', table.icon .. diagnostic.message, code)
+          end
+        end
+
+        return string.format('%s [%s]', diagnostic.message, code)
+      end
+
+      for _, table in pairs(codes) do
+        if vim.tbl_contains(table, code) then
+          return table.message
+        end
       end
 
       return string.format('%s [%s]', diagnostic.message, diagnostic.source)
@@ -30,6 +118,6 @@ for type, icon in pairs(signs) do
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
-for _,server_name in ipairs(EcoVim.lsp.servers) do
+for _, server_name in ipairs(EcoVim.lsp.servers) do
   require('lsp.servers.' .. server_name)
 end
