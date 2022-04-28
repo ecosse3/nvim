10e9a9b999d203a4eef96d041653e89a00a25108
ecosse3 <luk.kurpiewski@gmail.com>
2022-04-28 17:55:00 +0200

    fix(keymappings): pass opts to telescope project_files

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -112,8 +112,8 @@ M.edit_neovim = function()
   }
 end
 
-M.project_files = function()
-  local opts = {} -- define here if you want to define something
+M.project_files = function(opts)
+  opts = opts or {} -- define here if you want to define something
   local ok = pcall(require "telescope.builtin".git_files, opts)
   if not ok then require "telescope.builtin".find_files(opts) end
 end
