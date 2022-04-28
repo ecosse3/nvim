c979ddea8a04bdd9af4050f5fb9018d2b2a386f2
ecosse3 <luk.kurpiewski@gmail.com>
2022-04-28 08:44:48 +0200

    chore(functions): remove empty print

--- lua/functions.lua
+++ lua/functions.lua
@@ -12,7 +12,6 @@ vim.cmd([[
 local parsers = require'nvim-treesitter.parsers'
 local configs = parsers.get_parser_configs()
 local ft_str = table.concat(vim.tbl_map(function(ft) return configs[ft].filetype or ft end, parsers.available_parsers()), ',')
-print()
 vim.cmd('autocmd Filetype ' .. ft_str .. ' setlocal foldmethod=expr foldexpr=nvim_treesitter#foldexpr()')
 
 -- Exported functions
