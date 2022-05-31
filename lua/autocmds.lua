6eb9eac8605fb7904877e4d9514e3e7887735ae2
ecosse3 <luk.kurpiewski@gmail.com>
2022-05-31 23:41:12 +0200

    fix(winbar): enable winbar only for neovim 0.8+
    Fixes #50

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -11,35 +11,39 @@ vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, { pattern = { "*.txt", "*
 vim.api.nvim_create_autocmd("BufEnter", { pattern = "*.md", callback = function() require('plugins.which-key').attach_markdown(0) end })
 vim.api.nvim_create_autocmd("BufEnter", { pattern = { "*.ts", "*.tsx" }, callback = function() require('plugins.which-key').attach_typescript(0) end })
 vim.api.nvim_create_autocmd("BufEnter", { pattern = { "package.json" }, callback = function() require('plugins.which-key').attach_npm(0) end })
--- Winbar
-vim.api.nvim_create_autocmd({ "CursorMoved", "BufWinEnter", "BufFilePost" }, {
-  callback = function()
-    local winbar_filetype_exclude = {
-      "help",
-      "startify",
-      "dashboard",
-      "packer",
-      "neogitstatus",
-      "NvimTree",
-      "Trouble",
-      "alpha",
-      "lir",
-      "Outline",
-      "spectre_panel",
-      "toggleterm",
-    }
+vim.api.nvim_create_autocmd("BufEnter", { callback = function() if EcoVim.plugins.zen.enabled then require('plugins.which-key').attach_zen() end end })
+-- Winbar (for nvim 0.8+)
+if vim.fn.has('nvim-0.8') == 1 then
+  vim.api.nvim_create_autocmd({ "CursorMoved", "BufWinEnter", "BufFilePost" }, {
+    callback = function()
+      local winbar_filetype_exclude = {
+        "help",
+        "startify",
+        "dashboard",
+        "packer",
+        "neogitstatus",
+        "NvimTree",
+        "Trouble",
+        "alpha",
+        "lir",
+        "Outline",
+        "spectre_panel",
+        "toggleterm",
+        "TelescopePrompt"
+      }
 
-    if vim.tbl_contains(winbar_filetype_exclude, vim.bo.filetype) then
-      vim.opt_local.winbar = nil
-      return
-    end
+      if vim.tbl_contains(winbar_filetype_exclude, vim.bo.filetype) then
+        vim.opt_local.winbar = nil
+        return
+      end
 
-    local value = require("winbar").gps()
+      local value = require("winbar").gps()
 
-    if value == nil then
-      value = require("winbar").filename()
-    end
+      if value == nil then
+        value = require("winbar").filename()
+      end
 
-    vim.opt_local.winbar = value
-  end,
-})
+      vim.opt_local.winbar = value
+    end,
+  })
+end
