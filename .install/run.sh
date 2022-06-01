9e1b19324239535ebd26095e09d2167d356b5c9d
ecosse3 <luk.kurpiewski@gmail.com>
2022-06-01 18:22:03 +0200

    fix(installer): rm tmp file

--- .install/run.sh
+++ .install/run.sh
@@ -212,7 +212,6 @@ function install_packer() {
 }
 
 function finish () {
-  rm /tmp/ecovim-prevent-running-lsp-installer 
   touch /tmp/first-ecovim-run
   msg "${BOLD}${GREEN}Thank you for installing my ${BLUE}Ecovim${NC}${BOLD}${GREEN} config! Please support me by giving a star :)${NC}" 1
   echo -e "${BOLD}${GREEN}Do not forget to use a font with glyphs (icons) support [https://github.com/ryanoasis/nerd-fonts].\nI recommend Fira Code for Ecovim setup.${NC}"
@@ -234,7 +233,6 @@ function setup() {
   [ -f "$PACK_DIR/packer/start/packer.nvim/telescope-fzf-native.nvim/build/libfzf.so" ] && msg "${BOLD}${GREEN}Done${NC}" 1 0
   cd $CONFIG_DIR/.install
 
-  touch /tmp/ecovim-prevent-running-lsp-installer
   msg "${BOLD}Installing plugins...${NC}" 1
   nvim -c 'autocmd User PackerComplete quitall' \
     -c 'PackerSync'
