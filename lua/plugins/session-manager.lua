acb4003515379ac3636627a231b00562172d992c
ecosse3 <luk.kurpiewski@gmail.com>
2022-06-01 18:08:11 +0200

    Revert "fix(plugins): don't setup session manager at first launch"
    This reverts commit 3d17210cb82f6b5b73a525b968b7c9cd10a7241d.

--- lua/plugins/session-manager.lua
+++ lua/plugins/session-manager.lua
@@ -1,7 +1,3 @@
-local utils = require('utils')
-
-local is_first_plugin_installation = utils.file_exists('/tmp/ecovim-prevent-running-lsp-installer')
-
 local present, session_manager = pcall(require, "session_manager")
 if not present then
   return
@@ -12,18 +8,16 @@ if not path_present then
   return
 end
 
-if not is_first_plugin_installation then
-  session_manager.setup({
-    sessions_dir = Path:new(vim.fn.stdpath('data'), 'sessions'), -- The directory where the session files will be saved.
-    path_replacer = '__', -- The character to which the path separator will be replaced for session files.
-    colon_replacer = '++', -- The character to which the colon symbol will be replaced for session files.
-    autoload_mode = require('session_manager.config').AutoloadMode.Disabled, -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
-    autosave_last_session = true, -- Automatically save last session on exit and on session switch.
-    autosave_ignore_not_normal = true, -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
-    autosave_ignore_filetypes = { -- All buffers of these file types will be closed before the session is saved.
-      'gitcommit',
-    },
-    autosave_only_in_session = false, -- Always autosaves session. If true, only autosaves after a session is active.
-    max_path_length = 80,  -- Shorten the display path if length exceeds this threshold. Use 0 if don't want to shorten the path at all.
-  })
-end
+session_manager.setup({
+  sessions_dir = Path:new(vim.fn.stdpath('data'), 'sessions'), -- The directory where the session files will be saved.
+  path_replacer = '__', -- The character to which the path separator will be replaced for session files.
+  colon_replacer = '++', -- The character to which the colon symbol will be replaced for session files.
+  autoload_mode = require('session_manager.config').AutoloadMode.Disabled, -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
+  autosave_last_session = true, -- Automatically save last session on exit and on session switch.
+  autosave_ignore_not_normal = true, -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
+  autosave_ignore_filetypes = { -- All buffers of these file types will be closed before the session is saved.
+    'gitcommit',
+  },
+  autosave_only_in_session = false, -- Always autosaves session. If true, only autosaves after a session is active.
+  max_path_length = 80,  -- Shorten the display path if length exceeds this threshold. Use 0 if don't want to shorten the path at all.
+})
