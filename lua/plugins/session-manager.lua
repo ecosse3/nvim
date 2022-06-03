256ba840a0832a6475947d2e94a179d18ef00a17
ecosse3 <luk.kurpiewski@gmail.com>
2022-06-03 20:57:16 +0200

    fix(plugins): autosave only in session

--- lua/plugins/session-manager.lua
+++ lua/plugins/session-manager.lua
@@ -18,6 +18,6 @@ session_manager.setup({
   autosave_ignore_filetypes = { -- All buffers of these file types will be closed before the session is saved.
     'gitcommit',
   },
-  autosave_only_in_session = false, -- Always autosaves session. If true, only autosaves after a session is active.
+  autosave_only_in_session = true, -- Always autosaves session. If true, only autosaves after a session is active.
   max_path_length = 80,  -- Shorten the display path if length exceeds this threshold. Use 0 if don't want to shorten the path at all.
 })
