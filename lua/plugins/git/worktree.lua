c4aae3842f7381c48c1ff492511e08fd9defc357
ecosse3 <luk.kurpiewski@gmail.com>
2022-06-08 12:09:55 +0200

    fix(plugins): correct refreshing current file & close all buffers on worktree switch

--- lua/plugins/git/worktree.lua
+++ lua/plugins/git/worktree.lua
@@ -40,8 +40,22 @@ keymap("n", "<Leader>gwc", "<CMD>lua require('telescope').extensions.git_worktre
 -- ╭──────────────────────────────────────────────────────────╮
 -- │ Hooks                                                    │
 -- ╰──────────────────────────────────────────────────────────╯
+-- op = Operations.Switch, Operations.Create, Operations.Delete
+-- metadata = table of useful values (structure dependent on op)
+--      Switch
+--          path = path you switched to
+--          prev_path = previous worktree path
+--      Create
+--          path = path where worktree created
+--          branch = branch name
+--          upstream = upstream remote name
+--      Delete
+--          path = path where worktree deleted
+
 worktree.on_tree_change(function(op, metadata)
   if op == worktree.Operations.Switch then
     utils.log("Switched from " .. metadata.prev_path .. " to " .. metadata.path, "Git Worktree")
+    vim.cmd ('BufferCloseAllButCurrent')
+    vim.cmd ('e')
   end
 end)
