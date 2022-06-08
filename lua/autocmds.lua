7d081ff673ebaa1f4f60400898ac8480bd4f347a
ecosse3 <luk.kurpiewski@gmail.com>
2022-06-08 12:08:46 +0200

    fix(ui): vim.ui.select & vim.ui.input error when using nui backend with LSP rename/code actions

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -6,7 +6,7 @@ vim.api.nvim_create_autocmd("TextYankPost", { callback = function() vim.highligh
 vim.api.nvim_create_autocmd("BufRead", { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" })
 vim.api.nvim_create_autocmd("BufNewFile", { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" })
 -- Enable spell checking for certain file types
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, { pattern = { "*.txt", "*.md", "*.tex" }, command = "setlocal spell" })
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, { pattern = { "*.txt", "*.md", "*.tex" }, command = "setlocal spell" })
 -- Attach specific keybindings in which-key for specific filetypes
 vim.api.nvim_create_autocmd("BufEnter", { pattern = "*.md", callback = function() require('plugins.which-key').attach_markdown(0) end })
 vim.api.nvim_create_autocmd("BufEnter", { pattern = { "*.ts", "*.tsx" }, callback = function() require('plugins.which-key').attach_typescript(0) end })
@@ -29,9 +29,14 @@ if vim.fn.has('nvim-0.8') == 1 then
         "Outline",
         "spectre_panel",
         "toggleterm",
-        "TelescopePrompt"
+        "TelescopePrompt",
+        "DressingInput",
+        "DressingSelect",
       }
 
+      if (vim.api.nvim_win_get_config(0).relative ~= "") then
+        return
+      end
       if vim.tbl_contains(winbar_filetype_exclude, vim.bo.filetype) then
         vim.opt_local.winbar = nil
         return
--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -19,12 +19,12 @@ require('dressing').setup({
     relative = "cursor",
 
     -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-    prefer_width = 40,
+    prefer_width = 10,
     width = nil,
     -- min_width and max_width can be a list of mixed types.
     -- min_width = {20, 0.2} means "the greater of 20 columns or 20% of total"
     max_width = { 140, 0.9 },
-    min_width = { 20, 0.2 },
+    min_width = { 10, 0.1 },
 
     -- Window transparency (0-100)
     winblend = 0,
@@ -75,6 +75,7 @@ require('dressing').setup({
           nui = {
             relative = 'cursor',
             max_width = 80,
+            min_height = 2,
           }
         }
       end
