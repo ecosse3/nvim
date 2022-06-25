740b090457df67e159f7233dbd69d2873b570468
Kian Dadkhah <contact.kiands@gmail.com>
2022-06-24 20:18:43 -0400

    feat(lsp): create option to toggle inline messages

--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -22,6 +22,7 @@ local lspconfig = require("lspconfig")
 local handlers = {
   ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = EcoVim.ui.float.border }),
   ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = EcoVim.ui.float.border }),
+  ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = EcoVim.lsp.virtual_text }),
 }
 
 local function on_attach(client, bufnr)
