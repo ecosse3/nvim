d91b2499755171ca65a221238a2355cd70bebe56
ecosse3 <luk.kurpiewski@gmail.com>
2022-08-08 16:27:40 +0200

    fix(dressing): update dressing config & use builtin backend for code action since nui breaks

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -12,6 +12,9 @@ require('dressing').setup({
     -- When true, <Esc> will close the modal
     insert_only = true,
 
+    -- When true, input will start in insert mode.
+    start_in_insert = true,
+
     -- These are passed to nvim_open_win
     anchor = "SW",
     border = EcoVim.ui.float.border or "rounded",
@@ -30,6 +33,20 @@ require('dressing').setup({
     winblend = 0,
     -- Change default highlight groups (see :help winhl)
     winhighlight = "",
+
+    -- Set to `false` to disable
+    mappings = {
+      n = {
+        ["<Esc>"] = "Close",
+        ["<CR>"] = "Confirm",
+      },
+      i = {
+        ["<C-c>"] = "Close",
+        ["<CR>"] = "Confirm",
+        ["<Up>"] = "HistoryPrev",
+        ["<Down>"] = "HistoryNext",
+      },
+    },
 
     override = function(conf)
       -- This is the config that will be passed to nvim_open_win.
@@ -62,16 +79,56 @@ require('dressing').setup({
           top_align = "right",
         },
       },
+      buf_options = {
+        swapfile = false,
+        filetype = "DressingSelect",
+      },
       max_width = 80,
       max_height = 40,
     },
 
+    -- Options for built-in selector
+    builtin = {
+      -- These are passed to nvim_open_win
+      wnchor = "SW",
+      border = EcoVim.ui.float.border or "rounded",
+      -- 'editor' and 'win' will default to being centered
+      relative = "cursor",
+
+      -- Window transparency (0-100)
+      winblend = 5,
+      -- Change default highlight groups (see :help winhl)
+      winhighlight = "",
+
+      -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+      -- the min_ and max_ options can be a list of mixed types.
+      -- max_width = {140, 0.8} means "the lesser of 140 columns or 80% of total"
+      width = nil,
+      max_width = { 140, 0.8 },
+      min_width = { 10, 0.2 },
+      height = nil,
+      max_height = 0.9,
+      min_height = { 2, 0.05 },
+
+      -- Set to `false` to disable
+      mappings = {
+        ["<Esc>"] = "Close",
+        ["<C-c>"] = "Close",
+        ["<CR>"] = "Confirm",
+      },
+
+      override = function(conf)
+        -- This is the config that will be passed to nvim_open_win.
+        -- Change values here to customize the layout
+        return conf
+      end,
+    },
 
     -- see :help dressing_get_config
     get_config = function(opts)
       if opts.kind == 'codeaction' then
         return {
-          backend = 'nui',
+          backend = 'builtin',
           nui = {
             relative = 'cursor',
             max_width = 80,
