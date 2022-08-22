402d3316c8e927ad92706aad9d55143ea85f7ba0
ecosse3 <luk.kurpiewski@gmail.com>
2022-08-23 00:23:05 +0200

    style(plugins): format signs config

--- lua/plugins/git/signs.lua
+++ lua/plugins/git/signs.lua
@@ -7,37 +7,35 @@ end
 -- │ Setup                                                    │
 -- ╰──────────────────────────────────────────────────────────╯
 signs.setup {
-  signs                             = {
+  signs                        = {
     add          = { hl = 'GitGutterAdd', text = '▎', numhl = 'GitSignsAddNr' },
     change       = { hl = 'GitGutterChange', text = '▎', numhl = 'GitSignsChangeNr' },
     delete       = { hl = 'GitGutterDelete', text = '_', numhl = 'GitSignsDeleteNr' },
     topdelete    = { hl = 'GitGutterDelete', text = '‾', numhl = 'GitSignsDeleteNr' },
     changedelete = { hl = 'GitGutterChange', text = '~', numhl = 'GitSignsChangeNr' },
   },
-  signcolumn                        = true, -- Toggle with `:Gitsigns toggle_signs`
-  numhl                             = false, -- Toggle with `:Gitsigns toggle_numhl`
-  linehl                            = false, -- Toggle with `:Gitsigns toggle_linehl`
-  word_diff                         = false, -- Toggle with `:Gitsigns toggle_word_diff`
-  watch_gitdir                      = {
+  signcolumn                   = true, -- Toggle with `:Gitsigns toggle_signs`
+  numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
+  linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
+  word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
+  watch_gitdir                 = {
     interval = 700,
     follow_files = true
   },
-  attach_to_untracked               = true,
-  current_line_blame                = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-  current_line_blame_opts           = {
+  attach_to_untracked          = true,
+  current_line_blame           = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts      = {
     virt_text = true,
     virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
     delay = 700,
     ignore_whitespace = false,
   },
-  current_line_blame_formatter_opts = {
-    relative_time = false
-  },
-  sign_priority                     = 6,
-  update_debounce                   = 100,
-  status_formatter                  = nil, -- Use default
-  max_file_length                   = 40000,
-  preview_config                    = {
+  current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+  sign_priority                = 6,
+  update_debounce              = 100,
+  status_formatter             = nil, -- Use default
+  max_file_length              = 40000,
+  preview_config               = {
     -- Options passed to nvim_open_win
     border = EcoVim.ui.float.border,
     style = 'minimal',
@@ -45,10 +43,10 @@ signs.setup {
     row = 0,
     col = 1
   },
-  yadm                              = {
+  yadm                         = {
     enable = false
   },
-  on_attach                         = function(bufnr)
+  on_attach                    = function(bufnr)
     local gs = package.loaded.gitsigns
 
     local function map(mode, l, r, opts)
