ee9b83bfb833937002d9ba4fb79509704f833fc6
ecosse3 <luk.kurpiewski@gmail.com>
2022-08-23 00:23:29 +0200

    fix(plugins): fix going to prev/next git hunks by ]c and [c

--- lua/plugins/git/signs.lua
+++ lua/plugins/git/signs.lua
@@ -60,8 +60,17 @@ signs.setup {
     -- ╰──────────────────────────────────────────────────────────╯
 
     -- Navigation
-    map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
-    map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
+    map('n', ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, { expr = true })
+
+    map('n', '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, { expr = true })
 
     -- Actions
     map({ 'n', 'v' }, '<leader>ghs', gs.stage_hunk)
