2fba6eaf8717d6135c71f410cee3cc8ed6c4cbac
ecosse3 <luk.kurpiewski@gmail.com>
2022-09-07 17:17:47 +0200

    fix(plugins): fix colorizer.nvim config

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,5 +1,5 @@
-require 'colorizer'.setup (
-  {
+require 'colorizer'.setup({
+  filetypes = {
     'html',
     'css',
     'javascript',
@@ -8,17 +8,5 @@ require 'colorizer'.setup (
     'javascriptreact',
     'lua'
   },
-  {
-    RGB      = true;         -- #RGB hex codes
-    RRGGBB   = true;         -- #RRGGBB hex codes
-    names    = true;         -- "Name" codes like Blue or blue
-    RRGGBBAA = true;         -- #RRGGBBAA hex codes
-    rgb_fn   = true;         -- CSS rgb() and rgba() functions
-    hsl_fn   = true;         -- CSS hsl() and hsla() functions
-    css      = true;         -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-    css_fn   = true;         -- Enable all CSS *functions*: rgb_fn, hsl_fn
-
-    -- Available modes: foreground, background, virtualtext
-    mode = 'background'
-  }
-)
+  user_default_options = { mode = "background", }
+})
