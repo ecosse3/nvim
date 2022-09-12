9bbaac262ac8e4618dc8192a3bc07d431ed6c3c1
ecosse3 <luk.kurpiewski@gmail.com>
2022-09-12 10:38:28 +0200

    fix(plugins): update barbar to default branch cause wipeout-cmds was deleted

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -96,7 +96,7 @@ return require('packer').startup({
     use { 'ggandor/lightspeed.nvim', config = "require('plugins.lightspeed')" }
     use { 'folke/which-key.nvim', config = "require('plugins.which-key')", event = "BufWinEnter" }
     use { 'ecosse3/galaxyline.nvim', after = 'nvim-gps', config = "require('plugins.galaxyline')", event = "BufWinEnter" }
-    use { 'romgrk/barbar.nvim', branch = 'feat/wipeout-cmds', requires = { 'kyazdani42/nvim-web-devicons' },
+    use { 'romgrk/barbar.nvim', requires = { 'kyazdani42/nvim-web-devicons' },
       config = "require('plugins.barbar')" }
     use { 'antoinemadec/FixCursorHold.nvim' } -- Needed while issue https://github.com/neovim/neovim/issues/12587 is still open
     use { 'rcarriga/nvim-notify' }
