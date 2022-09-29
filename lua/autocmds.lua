af3a6fbf85dc8825de4a123ce73b35748a641453
ecosse3 <luk.kurpiewski@gmail.com>
2022-09-29 18:20:45 +0200

    feat(autocmds): decrease yank highlight timeout from 200ms to 100ms

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -2,7 +2,7 @@
 vim.api.nvim_create_autocmd("BufWritePost", { pattern = "plugins.lua", command = "source <afile> | PackerSync" })
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost",
-  { callback = function() vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 200 }) end })
+  { callback = function() vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 100 }) end })
 -- Disable diagnostics in node_modules (0 is current buffer only)
 vim.api.nvim_create_autocmd("BufRead", { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" })
 vim.api.nvim_create_autocmd("BufNewFile", { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" })
