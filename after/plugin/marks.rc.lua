7891c6822c4d36972672fdd362bf9e4ac6f57ad9
towry <towry@users.noreply.github.com>
2022-12-19 19:00:49 +0800

    up

--- after/plugin/marks.rc.lua
+++ after/plugin/marks.rc.lua
@@ -7,8 +7,9 @@ marks.setup({
   default_mappings = false,
   mappings = {
     set_next = "m,",
-    next = "m]",
+    next = "m<Space>",
     preview = "m:",
+    delete_buf = "dm<Space>",
     prev = false -- pass false to disable only this default mapping
   }
 })
