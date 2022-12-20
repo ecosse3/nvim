481fb6de88ab0a7e8a4323f4028a3f0434c5132a
towry <towry@users.noreply.github.com>
2022-12-20 10:05:13 +0800

    up

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -66,7 +66,7 @@ require('telescope').setup {
   },
   extensions = {
     fzf = {
-      override_generic_sorter = false,
+      override_generic_sorter = true,
       override_file_sorter = true,
       case_mode = "smart_case",
     },
