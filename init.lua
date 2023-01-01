84b79d21f94ee3a8c020d783459d21bdea9dd828
ecosse3 <luk.kurpiewski@gmail.com>
2023-01-01 16:39:13 +0100

    refactor(dirs): settings.lua -> config/options.lua

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
 require('utils.globals')
-require('settings')
 require('colorscheme')
 require('keymappings')
 require('autocmds')
 require('utils.functions')
 
 require('config.EcoVim')
+require('config.options')
 require('internal.cursorword')
 
 require('lsp.config')
