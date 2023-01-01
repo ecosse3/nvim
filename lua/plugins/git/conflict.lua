2a28a9714945a0ee224ddd6850ac2643047347f4
ecosse3 <luk.kurpiewski@gmail.com>
2023-01-01 16:42:30 +0100

    feat: disable diagnostics with git conflict in file

--- lua/plugins/git/conflict.lua
+++ lua/plugins/git/conflict.lua
@@ -5,7 +5,7 @@ end
 
 conflict.setup({
   default_mappings = true,     -- disable buffer local mapping created by this plugin
-  disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
+  disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
     highlights = {             -- They must have background color, otherwise the default color will be used
     incoming = 'DiffText',
     current = 'DiffAdd',
