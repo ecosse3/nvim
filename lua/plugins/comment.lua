6c40b079e7ae2bf06242b191e5e3c928a25c5769
ecosse3 <luk.kurpiewski@gmail.com>
2023-01-01 16:45:22 +0100

    style: comment lint

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -12,40 +12,43 @@ require('Comment').setup({
   ---Create basic (operator-pending) and extended mappings for NORMAL + VISUAL mode
   ---@type table
   mappings = {
-      ---operator-pending mapping
-      ---Includes `gcc`, `gcb`, `gc[count]{motion}` and `gb[count]{motion}`
-      basic = true,
-      ---extra mapping
-      ---Includes `gco`, `gcO`, `gcA`
-      extra = true,
-      ---extended mapping
-      ---Includes `g>`, `g<`, `g>[count]{motion}` and `g<[count]{motion}`
-      extended = false,
+    ---operator-pending mapping
+    ---Includes `gcc`, `gcb`, `gc[count]{motion}` and `gb[count]{motion}`
+    basic = true,
+    ---extra mapping
+    ---Includes `gco`, `gcO`, `gcA`
+    extra = true,
+    ---extended mapping
+    ---Includes `g>`, `g<`, `g>[count]{motion}` and `g<[count]{motion}`
+    extended = false,
   },
 
   ---LHS of toggle mapping in NORMAL + VISUAL mode
   ---@type table
   toggler = {
-      ---line-comment keymap
-      line = 'gcc',
-      ---block-comment keymap
-      block = 'gbc',
+    ---line-comment keymap
+    line = 'gcc',
+    ---block-comment keymap
+    block = 'gbc',
   },
 
   ---LHS of operator-pending mapping in NORMAL + VISUAL mode
   ---@type table
   opleader = {
-      ---line-comment keymap
-      line = 'gc',
-      ---block-comment keymap
-      block = 'gb',
+    ---line-comment keymap
+    line = 'gc',
+    ---block-comment keymap
+    block = 'gb',
   },
 
   ---Pre-hook, called before commenting the line
   ---@type function|nil
   ---@param ctx Ctx
+  -- pre_hook = function(ctx)
+  --   return require('ts_context_commentstring.internal').calculate_commentstring()
+  -- end,
   pre_hook = function(ctx)
-    return require('ts_context_commentstring.internal').calculate_commentstring()
+    return require('Comment.jsx').calculate(ctx)
   end,
 
   ---Post-hook, called after commenting is done
