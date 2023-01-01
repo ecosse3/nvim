7305db688b5f241d9eae3f5e3acfa65c0fd2b58c
ecosse3 <luk.kurpiewski@gmail.com>
2023-01-01 16:41:38 +0100

    style: functions lint

--- lua/lsp/functions.lua
+++ lua/lsp/functions.lua
@@ -5,7 +5,7 @@ function M.enable_format_on_save()
   vim.api.nvim_create_autocmd("BufWritePre", { callback = function()
     vim.lsp.buf.format()
   end,
-  group = group })
+    group = group })
   require('notify')("Enabled format on save", "info", { title = "LSP", timeout = 2000 })
 end
 
@@ -29,8 +29,10 @@ function M.custom_hover_handler(_, result)
   local handler = function(_, result)
     if result then
       local lines = vim.split(result.contents.value, '\n')
-      local bufnr = vim.lsp.util.open_floating_preview(lines, 'markdown', { border = EcoVim.ui.float.border or 'rounded' })
-      require('colorizer').highlight_buffer(bufnr, nil, vim.list_slice(lines, 2, #lines), 0, require('colorizer').get_buffer_options(0))
+      local bufnr = vim.lsp.util.open_floating_preview(lines, 'markdown',
+        { border = EcoVim.ui.float.border or 'rounded' })
+      require('colorizer').highlight_buffer(bufnr, nil, vim.list_slice(lines, 2, #lines), 0,
+        require('colorizer').get_buffer_options(0))
     end
   end
 
