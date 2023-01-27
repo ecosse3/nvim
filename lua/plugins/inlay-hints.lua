c75d448bce8669f1bcabc7c27b747cf18896d967
ecosse3 <luk.kurpiewski@gmail.com>
2023-01-27 05:13:54 +0100

    feat(plugins): add inlay-hints default config

---
+++ lua/plugins/inlay-hints.lua
@@ -0,0 +1,33 @@
+local default_config = {
+  inlay_hints = {
+    parameter_hints = {
+      show = true,
+      prefix = "<- ",
+      separator = ", ",
+      remove_colon_start = false,
+      remove_colon_end = true,
+    },
+    type_hints = {
+      -- type and other hints
+      show = true,
+      prefix = "",
+      separator = ", ",
+      remove_colon_start = false,
+      remove_colon_end = false,
+    },
+    only_current_line = false,
+    -- separator between types and parameter hints. Note that type hints are
+    -- shown before parameter
+    labels_separator = "  ",
+    -- whether to align to the length of the longest line in the file
+    max_len_align = false,
+    -- padding from the left if max_len_align is true
+    max_len_align_padding = 1,
+    -- highlight group
+    highlight = "LspCodeLens",
+  },
+  enabled_at_startup = true,
+  debug_mode = false,
+}
+
+require("lsp-inlayhints").setup(default_config)
