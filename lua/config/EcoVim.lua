2d72bf433fc26f3b3fdc24fbdf7b0a7bdc034033
ecosse3 <luk.kurpiewski@gmail.com>
2023-03-13 15:38:17 +0100

    style: lint EcoVim.lua

--- lua/config/EcoVim.lua
+++ lua/config/EcoVim.lua
@@ -1,44 +1,43 @@
 ------------------------------------------------
 --                                            --
---    This is a main configuation file for    --
+--    This is a main configuration file for    --
 --                    EcoVim                  --
 --      Change variables which you need to    --
 --                                            --
 ------------------------------------------------
 
-local icons = require('utils.icons')
+local icons = require("utils.icons")
 
 EcoVim = {
-  colorscheme = 'tokyonight',
-  ui = {
-    float = {
-      border = 'rounded'
-    }
-  },
-  plugins = {
-    completion = {
-      select_first_on_enter = false
-    },
-    copilot = {
-      enabled = false,
-    },
-    rooter = {
-      -- Removing package.json from list in Monorepo Frontend Project can be helpful
-      -- By that your live_grep will work related to whole project, not specific package
-      patterns = {'.git', 'package.json', '_darcs', '.bzr', '.svn', 'Makefile'} -- Default
-    },
-    zen = {
-      kitty_enabled = true,
-      enabled = true, -- sync after change
-    },
-  },
-  icons = icons,
-  statusline = {
-    path_enabled = false,
-    path = 'relative' -- absolute/relative
-  },
-  lsp = {
-    virtual_text = true, -- show virtual text (errors, warnings, info) inline messages
-  },
+	colorscheme = "tokyonight",
+	ui = {
+		float = {
+			border = "rounded",
+		},
+	},
+	plugins = {
+		completion = {
+			select_first_on_enter = false,
+		},
+		copilot = {
+			enabled = true,
+		},
+		rooter = {
+			-- Removing package.json from list in Monorepo Frontend Project can be helpful
+			-- By that your live_grep will work related to whole project, not specific package
+			patterns = { ".git", "package.json", "_darcs", ".bzr", ".svn", "Makefile" }, -- Default
+		},
+		zen = {
+			kitty_enabled = true,
+			enabled = true, -- sync after change
+		},
+	},
+	icons = icons,
+	statusline = {
+		path_enabled = false,
+		path = "relative", -- absolute/relative
+	},
+	lsp = {
+		virtual_text = true, -- show virtual text (errors, warnings, info) inline messages
+	},
 }
-
