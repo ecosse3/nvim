e98989d2b068c48c333878bd9f51004b7acdc4ca
ecosse3 <luk.kurpiewski@gmail.com>
2023-03-13 15:26:03 +0100

    feat(plugins): add autocmd to notify when conflict is detected in git file

--- lua/plugins/git/conflict.lua
+++ lua/plugins/git/conflict.lua
@@ -1,13 +1,21 @@
 local present, conflict = pcall(require, "git-conflict")
 if not present then
-  return
+	return
 end
 
 conflict.setup({
-  default_mappings = true,     -- disable buffer local mapping created by this plugin
-  disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
-    highlights = {             -- They must have background color, otherwise the default color will be used
-    incoming = 'DiffText',
-    current = 'DiffAdd',
-  }
+	default_mappings = true, -- disable buffer local mapping created by this plugin
+	default_commands = true, -- disable commands created by this plugin
+	disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
+	highlights = { -- They must have background color, otherwise the default color will be used
+		incoming = "DiffText",
+		current = "DiffAdd",
+	},
+})
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "GitConflictDetected",
+	callback = function()
+		vim.notify("Conflict detected in " .. vim.fn.expand("<afile>"))
+	end,
 })
