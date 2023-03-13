7a7168ba4986bbcafae17270939c8be54063bbc7
ecosse3 <luk.kurpiewski@gmail.com>
2023-03-13 15:41:43 +0100

    feat(plugins): add inlay hints banned notify message

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -317,7 +317,11 @@ return {
 			})
 		end,
 		init = function()
-			local banned_messages = { "No information available", "LSP[tsserver] Inlay Hints request failed. Requires TypeScript 4.4+." }
+			local banned_messages = {
+				"No information available",
+				"LSP[tsserver] Inlay Hints request failed. Requires TypeScript 4.4+.",
+				"LSP[tsserver] Inlay Hints request failed. File not opened in the editor.",
+			}
 			vim.notify = function(msg, ...)
 				for _, banned in ipairs(banned_messages) do
 					if msg == banned then
