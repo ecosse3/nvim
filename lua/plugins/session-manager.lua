61a20f788f70a9d2d8b14fd690ea5aa9fec46c6f
ecosse3 <luk.kurpiewski@gmail.com>
2023-03-17 08:32:11 +0100

    fix(plugins): use new api of nvim-tree in session-manager

--- lua/plugins/session-manager.lua
+++ lua/plugins/session-manager.lua
@@ -28,7 +28,7 @@ vim.api.nvim_create_autocmd({ 'User' }, {
   pattern = "SessionLoadPost",
   group = config_group,
   callback = function()
-    require('nvim-tree').toggle(false, true)
+    require('nvim-tree.api').tree.toggle(false, true)
     require('notify')('Session loaded!', 'info', { title = 'Session Manager' })
   end,
 })
@@ -38,7 +38,7 @@ vim.api.nvim_create_autocmd({ 'User' }, {
   group = config_group,
   callback = function()
     require('notify')('Session saved!', 'info', { title = 'Session Manager', bufid = 0 })
-    require('nvim-tree').toggle(false, true)
+    require('nvim-tree.api').tree.toggle(false, true)
   end,
 })
 
