fe83f5df46c71721daab3e8802436d9192adfe58
ecosse3 <luk.kurpiewski@gmail.com>
2023-03-17 08:30:41 +0100

    fix(plugins): leep default keymaps of ChatGPT cause new versions breaks custom ones

--- lua/plugins/chat-gpt.lua
+++ lua/plugins/chat-gpt.lua
@@ -1,11 +1,5 @@
 require("chatgpt").setup({
   keymaps = {
     close = { "<C-c>", "<Esc>" },
-    yank_last = "<C-y>",
-    scroll_up = "<C-u>",
-    scroll_down = "<C-d>",
-    toggle_settings = "<C-o>",
-    new_session = "<C-m>",
-    cycle_windows = "<Tab>",
   },
 })
