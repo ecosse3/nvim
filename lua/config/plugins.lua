cfd75fda8a0445d68092ecaf2bd2b765dda4fa42
ecosse3 <luk.kurpiewski@gmail.com>
2023-03-24 17:57:20 +0100

    fix(plugins): trigger nvim-tree via cmds

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -65,6 +65,13 @@ return {
 	},
 	{
 		"nvim-tree/nvim-tree.lua",
+		cmd = {
+			"NvimTreeOpen",
+			"NvimTreeClose",
+			"NvimTreeToggle",
+			"NvimTreeFindFile",
+			"NvimTreeFindFileToggle",
+		},
 		keys = {
 			{ "<C-e>", "<cmd>lua require('nvim-tree.api').tree.toggle()<CR>", desc = "NvimTree" },
 		},
