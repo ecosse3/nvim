7f157212bce8aa2ec00234cfa886f7fe9c7c6b52
ecosse3 <luk.kurpiewski@gmail.com>
2023-04-04 18:35:16 +0200

    feat(lsp): enable TypeScript codeactions for null-ls by default

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,10 +1,14 @@
 require("mason-null-ls").setup({
-    ensure_installed = {
-        -- Opt to list sources here, when available in mason.
-    },
-    automatic_installation = false,
-    automatic_setup = true, -- Recommended, but optional
+  ensure_installed = {
+    -- Opt to list sources here, when available in mason.
+  },
+  automatic_installation = false,
+  automatic_setup = true, -- Recommended, but optional
 })
 
-require("null-ls").setup()
-require "mason-null-ls".setup_handlers() -- If `automatic_setup` is true.
+require("null-ls").setup({
+  sources = {
+    require("typescript.extensions.null-ls.code-actions"),
+  },
+})
+require("mason-null-ls").setup_handlers() -- If `automatic_setup` is true.
