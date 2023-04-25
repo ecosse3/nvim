9494b6fb54620424dcc0ab75e7f23c3dbadeb01d
ecosse3 <luk.kurpiewski@gmail.com>
2023-04-25 15:08:28 +0200

    feat(plugins): update nvim-tree & make "File Explorer" title always align in the middle even when resized

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,8 +1,10 @@
 local utils = require("utils")
-local nvim_tree_events = require("nvim-tree.events")
 local bufferline_api = require("bufferline.api")
+local api = require("nvim-tree.api")
+local Event = api.events.Event
 
 local TREE_WIDTH = 40
+local current_size = nil
 
 local git_icons = {
 	unstaged = "",
@@ -14,47 +16,64 @@ local git_icons = {
 	ignored = "◌",
 }
 
-local keymappings = {
-	{ key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
-	-- <C-e> keymapping cannot be set because it's used for toggling nvim-tree
-	-- { key = "<C-e>",                        action = "edit_in_place" },
-	{ key = { "O" }, action = "edit_no_picker" },
-	{ key = { "<2-RightMouse>", "<C-]>" }, action = "cd" },
-	{ key = "<C-v>", action = "vsplit" },
-	{ key = "<C-x>", action = "split" },
-	{ key = "<C-t>", action = "tabnew" },
-	{ key = "<", action = "prev_sibling" },
-	{ key = ">", action = "next_sibling" },
-	{ key = "P", action = "parent_node" },
-	{ key = "<BS>", action = "close_node" },
-	{ key = "<Tab>", action = "preview" },
-	{ key = "K", action = "first_sibling" },
-	{ key = "J", action = "last_sibling" },
-	{ key = "I", action = "toggle_ignored" },
-	{ key = "H", action = "toggle_dotfiles" },
-	{ key = "R", action = "refresh" },
-	{ key = "a", action = "create" },
-	{ key = "d", action = "remove" },
-	{ key = "D", action = "trash" },
-	{ key = "r", action = "rename" },
-	{ key = "<C-r>", action = "full_rename" },
-	{ key = "x", action = "cut" },
-	{ key = "c", action = "copy" },
-	{ key = "p", action = "paste" },
-	{ key = "y", action = "copy_name" },
-	{ key = "Y", action = "copy_path" },
-	{ key = "gy", action = "copy_absolute_path" },
-	{ key = "[c", action = "prev_git_item" },
-	{ key = "]c", action = "next_git_item" },
-	{ key = "-", action = "dir_up" },
-	{ key = "s", action = "system_open" },
-	{ key = "q", action = "close" },
-	{ key = "g?", action = "toggle_help" },
-	{ key = "W", action = "collapse_all" },
-	{ key = "S", action = "search_node" },
-}
+local function on_attach(bufnr)
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+
+  -- Default mappings not inserted as:
+  --  remove_keymaps = true
+  --  OR
+  --  view.mappings.custom_only = true
+
+
+  -- Mappings migrated from view.mappings.list
+  --
+  -- You will need to insert "your code goes here" for any mappings with a custom action_cb
+  vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', '<2-LeftMouse>', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', 'O', api.node.open.no_window_picker, opts('Open: No Window Picker'))
+  vim.keymap.set('n', '<2-RightMouse>', api.tree.change_root_to_node, opts('CD'))
+  vim.keymap.set('n', '<C-]>', api.tree.change_root_to_node, opts('CD'))
+  vim.keymap.set('n', '<C-v>', api.node.open.vertical, opts('Open: Vertical Split'))
+  vim.keymap.set('n', '<C-x>', api.node.open.horizontal, opts('Open: Horizontal Split'))
+  vim.keymap.set('n', '<C-t>', api.node.open.tab, opts('Open: New Tab'))
+  vim.keymap.set('n', '<', api.node.navigate.sibling.prev, opts('Previous Sibling'))
+  vim.keymap.set('n', '>', api.node.navigate.sibling.next, opts('Next Sibling'))
+  vim.keymap.set('n', 'P', api.node.navigate.parent, opts('Parent Directory'))
+  vim.keymap.set('n', '<BS>', api.node.navigate.parent_close, opts('Close Directory'))
+  vim.keymap.set('n', '<Tab>', api.node.open.preview, opts('Open Preview'))
+  vim.keymap.set('n', 'K', api.node.navigate.sibling.first, opts('First Sibling'))
+  vim.keymap.set('n', 'J', api.node.navigate.sibling.last, opts('Last Sibling'))
+  vim.keymap.set('n', 'I', api.tree.toggle_gitignore_filter, opts('Toggle Git Ignore'))
+  vim.keymap.set('n', 'H', api.tree.toggle_hidden_filter, opts('Toggle Dotfiles'))
+  vim.keymap.set('n', 'R', api.tree.reload, opts('Refresh'))
+  vim.keymap.set('n', 'a', api.fs.create, opts('Create'))
+  vim.keymap.set('n', 'd', api.fs.remove, opts('Delete'))
+  vim.keymap.set('n', 'D', api.fs.trash, opts('Trash'))
+  vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
+  vim.keymap.set('n', '<C-r>', api.fs.rename_sub, opts('Rename: Omit Filename'))
+  vim.keymap.set('n', 'x', api.fs.cut, opts('Cut'))
+  vim.keymap.set('n', 'c', api.fs.copy.node, opts('Copy'))
+  vim.keymap.set('n', 'p', api.fs.paste, opts('Paste'))
+  vim.keymap.set('n', 'y', api.fs.copy.filename, opts('Copy Name'))
+  vim.keymap.set('n', 'Y', api.fs.copy.relative_path, opts('Copy Relative Path'))
+  vim.keymap.set('n', 'gy', api.fs.copy.absolute_path, opts('Copy Absolute Path'))
+  vim.keymap.set('n', '[c', api.node.navigate.git.prev, opts('Prev Git'))
+  vim.keymap.set('n', ']c', api.node.navigate.git.next, opts('Next Git'))
+  vim.keymap.set('n', '-', api.tree.change_root_to_parent, opts('Up'))
+  vim.keymap.set('n', 's', api.node.run.system, opts('Run System'))
+  vim.keymap.set('n', 'q', api.tree.close, opts('Close'))
+  vim.keymap.set('n', 'g?', api.tree.toggle_help, opts('Help'))
+  vim.keymap.set('n', 'W', api.tree.collapse_all, opts('Collapse'))
+  vim.keymap.set('n', 'S', api.tree.search_node, opts('Search'))
+
+end
 
 require("nvim-tree").setup({
+  on_attach = on_attach,
 	-- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
 	sync_root_with_cwd = true,
 	--false by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree
@@ -142,8 +161,6 @@ require("nvim-tree").setup({
 			-- custom only false will merge the list with the default mappings
 			-- if true, it will only use your list to set the mappings
 			custom_only = true,
-			-- list of mappings to set on the tree manually
-			list = keymappings,
 		},
 		number = false,
 		relativenumber = false,
@@ -157,10 +174,18 @@ vim.api.nvim_set_keymap(
 	{ noremap = true, silent = true }
 )
 
-nvim_tree_events.on_tree_open(function()
-	bufferline_api.set_offset(TREE_WIDTH + 1, utils.add_whitespaces(13) .. "File Explorer")
+api.events.subscribe(Event.TreeOpen, function()
+	bufferline_api.set_offset(
+		current_size or TREE_WIDTH + 1,
+		utils.add_whitespaces(((current_size or TREE_WIDTH + 1) - 13) / 2) .. "File Explorer"
+	)
 end)
 
-nvim_tree_events.on_tree_close(function()
+api.events.subscribe(Event.Resize, function(size)
+	bufferline_api.set_offset(size + 1, utils.add_whitespaces((size + 1 - 13) / 2) .. "File Explorer")
+	current_size = size + 1
+end)
+
+api.events.subscribe(Event.TreeClose, function(data)
 	bufferline_api.set_offset(0)
 end)
