b0c58b1cd77b9a4f51affb29ee75dd50361dfa40
ecosse3 <luk.kurpiewski@gmail.com>
2023-04-28 11:27:38 +0200

    feat(plugins): add list of base languages to ensure_installed in treesitter
    Closes #135

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,21 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {},          -- one of "all", or a list of languages
+  ensure_installed = {
+    "tsx",
+    "typescript",
+    "javascript",
+    "html",
+    "css",
+    "vue",
+    "astro",
+    "svelte",
+    "gitcommit",
+    "graphql",
+    "json",
+    "json5",
+    "lua",
+    "markdown",
+    "prisma",
+  },          -- one of "all", or a list of languages
   sync_install = false,           -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "haskell" }, -- list of parsers to ignore installing
   highlight = {
