60e737c8cbfdbe18bc78253dc2f50f837817bdbe
ecosse3 <luk.kurpiewski@gmail.com>
2023-05-08 20:10:00 +0200

    feat(ui): link LspInfoBorder to FloatBoder in tokyonight.nvim

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -89,6 +89,7 @@ if EcoVim.colorscheme == "tokyonight" then
   vim.api.nvim_set_hl(0, "VertSplit", { bg = "#16161e", fg = "#16161e" })
   vim.api.nvim_set_hl(0, "BqfPreviewBorder", { link = "FloatBorder" })
   vim.api.nvim_set_hl(0, "BufferInactiveIndex", { link = "BufferInactive" })
+  vim.api.nvim_set_hl(0, "LspInfoBorder", { link = "FloatBorder" })
 
   -- Tree
   vim.api.nvim_set_hl(0, "NvimTreeFolderIcon", { bg = "None", fg = "None" })
