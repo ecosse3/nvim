7c0dbb771c38ff3e8a991b736ae5c9646794e007
ecosse3 <luk.kurpiewski@gmail.com>
2023-05-27 12:16:38 +0200

    style: lint bufferline config

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,28 +1,34 @@
 local bufferline = require("bufferline")
 
 bufferline.setup({
-	options = {
-		close_command = function(n)
-			require("mini.bufremove").delete(n, false)
-		end,
-		right_mouse_command = function(n)
-			require("mini.bufremove").delete(n, false)
-		end,
-		show_buffer_close_icons = false,
-		separator_style = { "|", "|" },
-		always_show_bufferline = true,
-		style_preset = bufferline.style_preset.no_italic,
-		numbers = function(opts)
-			return string.format("%s", opts.ordinal)
-		end,
-		offsets = {
-			{
-				filetype = "NvimTree",
-				text = "File Explorer",
-				highlight = "EcovimNvimTreeTitle",
-				text_align = "center",
-				separator = true,
-			},
-		},
-	},
+  options = {
+    close_command = function(n)
+      require("mini.bufremove").delete(n, false)
+    end,
+    right_mouse_command = function(n)
+      require("mini.bufremove").delete(n, false)
+    end,
+    show_buffer_close_icons = false,
+    separator_style = { "|", "|" },
+    always_show_bufferline = true,
+    style_preset = bufferline.style_preset.no_italic,
+    numbers = function(opts)
+      return string.format("%s", opts.ordinal)
+    end,
+    custom_filter = function(buf_number)
+      -- filter out filetypes you don't want to see
+      if vim.bo[buf_number].filetype ~= "qf" then
+        return true
+      end
+    end,
+    offsets = {
+      {
+        filetype = "NvimTree",
+        text = "File Explorer",
+        highlight = "EcovimNvimTreeTitle",
+        text_align = "center",
+        separator = true,
+      },
+    },
+  },
 })
