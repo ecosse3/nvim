ba2b9fd663dd390f43d0c70b41be6a8e63fedf2d
ecosse3 <luk.kurpiewski@gmail.com>
2023-07-10 20:54:02 +0200

    chore(autocmds): concatenate two autocmd into one

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,8 +2,7 @@
 vim.api.nvim_create_autocmd("TextYankPost",
   { callback = function() vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 100 }) end })
 -- Disable diagnostics in node_modules (0 is current buffer only)
-vim.api.nvim_create_autocmd("BufRead", { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" })
-vim.api.nvim_create_autocmd("BufNewFile", { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" })
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" })
 -- Enable spell checking for certain file types
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, { pattern = { "*.txt", "*.md", "*.tex" },
   command = "setlocal spell" })
