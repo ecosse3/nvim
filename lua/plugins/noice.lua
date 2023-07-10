7df3d8a21a5c73e69f57bfc5c4d40474de53eeda
ecosse3 <luk.kurpiewski@gmail.com>
2023-07-10 20:55:26 +0200

    style: align comments in noice.lua

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -16,10 +16,10 @@ require("noice").setup({
   },
   -- you can enable a preset for easier configuration
   presets = {
-    bottom_search = false,      -- use a classic bottom cmdline for search
-    command_palette = true,     -- position the cmdline and popupmenu together
+    bottom_search = false,        -- use a classic bottom cmdline for search
+    command_palette = true,       -- position the cmdline and popupmenu together
     long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = false,         -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = false,     -- add a border to hover docs and signature help
+    inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false,       -- add a border to hover docs and signature help
   },
 })
