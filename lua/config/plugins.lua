711731c5a9309210711b290e2f4bced8afa58f6b
ecosse3 <luk.kurpiewski@gmail.com>
2023-09-09 11:57:40 +0200

    fix(plugins): nvim-surround loading

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -470,7 +470,8 @@ return {
   },
   {
     "kylechui/nvim-surround",
-    lazy = false,
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
     config = true,
   },
   {
