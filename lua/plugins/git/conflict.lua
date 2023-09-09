5ebe6a7bed809620d0884eab5744f2aaaff12f78
ecosse3 <luk.kurpiewski@gmail.com>
2023-09-09 11:55:03 +0200

    style: lint git/conflict.lua

--- lua/plugins/git/conflict.lua
+++ lua/plugins/git/conflict.lua
@@ -4,10 +4,10 @@ if not present then
 end
 
 conflict.setup({
-	default_mappings = true, -- disable buffer local mapping created by this plugin
-	default_commands = true, -- disable commands created by this plugin
-	disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
-	highlights = { -- They must have background color, otherwise the default color will be used
+	default_mappings = true,                           -- disable buffer local mapping created by this plugin
+	default_commands = true,                           -- disable commands created by this plugin
+	disable_diagnostics = true,                        -- This will disable the diagnostics in a buffer whilst it is conflicted
+	highlights = {                                     -- They must have background color, otherwise the default color will be used
 		incoming = "DiffText",
 		current = "DiffAdd",
 	},
