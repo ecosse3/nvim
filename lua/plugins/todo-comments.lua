73d8fc259fd83c09c125cefe3b565a8945dcf873
ecosse3 <luk.kurpiewski@gmail.com>
2023-09-29 22:08:58 +0200

    feat: improve todo comments bg adding foreground highlight for all text

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -19,9 +19,8 @@ todo_comments.setup {
   },
   highlight = {
     before = "", -- "fg" or "bg" or empty
-    -- keyword = "wide", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
     keyword = "wide", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
-    after = "", -- "fg" or "bg" or empty
+    after = "fg", -- "fg" or "bg" or empty
     pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
     comments_only = true, -- uses treesitter to match keywords in comments only
     max_line_len = 400, -- ignore lines longer than this
