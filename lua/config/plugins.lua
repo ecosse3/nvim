e80e0f7f233ff1c01e790f559563fa87977d6137
ecosse3 <luk.kurpiewski@gmail.com>
2023-09-29 22:22:28 +0200

    feat(ui): scale down lazygit window to 0.9 scaling factor

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -745,7 +745,7 @@ return {
       { "<Leader>gg", "<cmd>LazyGit<CR>", desc = "lazygit" },
     },
     config = function()
-      vim.g.lazygit_floating_window_scaling_factor = 1
+      vim.g.lazygit_floating_window_scaling_factor = 0.9
     end,
   },
   {
