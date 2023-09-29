f828733d868c566303e105070c90aced6a848445
ecosse3 <luk.kurpiewski@gmail.com>
2023-09-29 22:12:56 +0200

    style(functions): lint

--- lua/lsp/functions.lua
+++ lua/lsp/functions.lua
@@ -31,34 +31,34 @@ function M.enable_format_on_save()
 end
 
 function M.disable_format_on_save()
-	vim.api.nvim_del_augroup_by_name("format_on_save")
-	require("notify")("Disabled format on save", "info", { title = "LSP", timeout = 2000 })
+  vim.api.nvim_del_augroup_by_name("format_on_save")
+  require("notify")("Disabled format on save", "info", { title = "LSP", timeout = 2000 })
 end
 
 function M.toggle_format_on_save()
-	if vim.fn.exists("#format_on_save#BufWritePre") == 0 then
-		M.enable_format_on_save()
-	else
-		M.disable_format_on_save()
-	end
+  if vim.fn.exists("#format_on_save#BufWritePre") == 0 then
+    M.enable_format_on_save()
+  else
+    M.disable_format_on_save()
+  end
 end
 
 vim.api.nvim_create_user_command("LspToggleAutoFormat", 'lua require("lsp.functions").toggle_format_on_save()', {})
 
 -- Custom textDocument/hover LSP handler to colorize colors inside hover results - WIP
 function M.custom_hover_handler(_, result)
-	local handler = function(_, result)
-		if result then
-			local colorizer = require("colorizer")
+  local handler = function(_, result)
+    if result then
+      local colorizer = require("colorizer")
 
-			local lines = vim.split(result.contents.value, "\n")
-			local bufnr =
-				vim.lsp.util.open_floating_preview(lines, "markdown", { border = EcoVim.ui.float.border or "rounded" })
-			colorizer.highlight_buffer(bufnr, nil, vim.list_slice(lines, 2, #lines), 0, colorizer.get_buffer_options(0))
-		end
-	end
+      local lines = vim.split(result.contents.value, "\n")
+      local bufnr =
+          vim.lsp.util.open_floating_preview(lines, "markdown", { border = EcoVim.ui.float.border or "rounded" })
+      colorizer.highlight_buffer(bufnr, nil, vim.list_slice(lines, 2, #lines), 0, colorizer.get_buffer_options(0))
+    end
+  end
 
-	return handler
+  return handler
 end
 
 return M
