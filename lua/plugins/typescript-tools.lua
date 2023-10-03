b36ae67bc6ba67ca469f188a04a7203e68bcf960
ecosse3 <luk.kurpiewski@gmail.com>
2023-10-04 00:05:23 +0200

    chore(typescript): remove mason registry since it's managed automatically & add expierimental codelens off by default

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -3,9 +3,6 @@ local baseDefinitionHandler = vim.lsp.handlers["textDocument/definition"]
 local filter = require("lsp.utils.filter").filter
 local filterReactDTS = require("lsp.utils.filterReactDTS").filterReactDTS
 
-local mason_registry = require('mason-registry')
-local tsserver_path = mason_registry.get_package('typescript-language-server'):get_install_path()
-
 local handlers = {
   ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
     silent = true,
@@ -37,6 +34,7 @@ require("typescript-tools").setup({
   handlers = handlers,
   settings = {
     separate_diagnostic_server = true,
+    code_lens = "off",
     tsserver_file_preferences = {
       includeInlayParameterNameHints = "all",
       includeCompletionsForModuleExports = true,
