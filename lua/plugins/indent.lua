85e8940fa2d3d9603ccf203883a383910724242c
ecosse3 <luk.kurpiewski@gmail.com>
2023-10-04 00:04:19 +0200

    fix(plugins): fix indent-blankline.nvim setup options

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -15,9 +15,6 @@ local highlight = {
 }
 
 require("ibl").setup {
-  show_current_context = true,
-  show_current_context_start = false,
-  show_end_of_line = false,
   exclude = {
     filetypes = { "help", "dashboard", "packer", "NvimTree", "Trouble", "TelescopePrompt", "Float" },
     buftypes = { "terminal", "nofile", "telescope" },
