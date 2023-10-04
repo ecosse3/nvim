184b9faf61c86e4faf040ef629b794842b78c07f
ecosse3 <luk.kurpiewski@gmail.com>
2023-10-04 11:37:36 +0200

    lint: remove unused indent highlights

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -4,16 +4,6 @@ vim.opt.list = true
 -- vim.opt.listchars:append("space:⋅")
 -- vim.opt.listchars:append("eol:↴")
 
-local highlight = {
-  "rainbowcol1",
-  "rainbowcol2",
-  "rainbowcol3",
-  "rainbowcol4",
-  "rainbowcol5",
-  "rainbowcol6",
-  "rainbowcol7",
-}
-
 require("ibl").setup {
   exclude = {
     filetypes = { "help", "dashboard", "packer", "NvimTree", "Trouble", "TelescopePrompt", "Float" },
