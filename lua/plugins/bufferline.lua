85dbabe60687a8926bac7d08b81212a72ce6d1c2
Erwin van Eijk <235739+erwinvaneijk@users.noreply.github.com>
2024-01-01 14:03:54 +0100

    fix(bufferline): change setup
    - change the looks
    - add indicator for LSP messages

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -8,13 +8,29 @@ bufferline.setup({
     right_mouse_command = function(n)
       require("mini.bufremove").delete(n, false)
     end,
+    buffer_close_icon = '󰅖',
+    modified_icon = '●',
+    close_icon = '',
+    left_trunc_marker = '',
+    right_trunc_marker = '',
     show_buffer_close_icons = false,
-    separator_style = { "|", "|" },
+    separator_style = "slant", -- { "|", "|" },
     always_show_bufferline = true,
-    style_preset = bufferline.style_preset.no_italic,
+    diagnostics = "nvim_lsp",
+    -- luacheck: push ignore unused-local
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+      local icon = level:match("error") and " " or " "
+      return " " .. icon .. count
+    end,
+    style_preset = bufferline.style_preset.default,
     numbers = function(opts)
       return string.format("%s", opts.ordinal)
     end,
+    hover = {
+      enabled = true,
+      delay = 250,
+      reveal = { 'close' }
+    },
     custom_filter = function(buf_number)
       -- filter out filetypes you don't want to see
       if vim.bo[buf_number].filetype ~= "qf" then
