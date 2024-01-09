5cb65825ebf7ec7e3ecf8a9481b5ed63dc755f21
Sergei Garin <sergey@garin.dev>
2024-01-09 12:34:48 +0300

    hide tabs, try fix md plugin install

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -462,7 +462,7 @@ return {
   },
   {
     "iamcco/markdown-preview.nvim",
-    build = "cd app && npm install",
+    build = "cd app && npm ci",
     setup = function()
       vim.g.mkdp_filetypes = { "markdown" }
     end,
