ec42ddc19c3230b0e71d9f5a8b6a2239b8e5bf5e
ecosse3 <luk.kurpiewski@gmail.com>
2024-02-05 07:27:32 +0100

    fix(typescript): correctly enable inlay hints lsp for typescript

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -28,7 +28,7 @@ require("typescript-tools").setup({
   on_attach = function(client, bufnr)
     if vim.fn.has("nvim-0.10") then
       -- Enable inlay hints
-      vim.lsp.inlay_hint(bufnr, true)
+      vim.lsp.inlay_hint.enable(bufnr, true)
     end
   end,
   handlers = handlers,
