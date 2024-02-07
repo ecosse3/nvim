91e36181dbd7b082d2fbb5adaa9a561206db9360
ecosse3 <luk.kurpiewski@gmail.com>
2024-02-07 17:26:22 +0100

    feat(plugins): update button keybindings for Alpha plugin

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -112,13 +112,13 @@ end
 
 dashboard.section.buttons.val = {
   button("<C-P>",     icons.fileNoBg .. " " .. "Find File",             "<cmd>lua require('plugins.telescope').project_files()<CR>",    {}),
-  button("<S-P>",     icons.word .. " " .. "Find Word",                 "<cmd>lua require('plugins.telescope.pickers.multi-rg')()<CR>", {}),
-  button("SPC s h",   icons.fileRecent .. " " .. "Recents",             "<cmd>Telescope oldfiles hidden=true<CR>",                      {}),
-  button("SPC / s d", icons.timer .. " " .. "Load Current Dir Session", "<cmd>SessionManager load_current_dir_session<CR>",             {}),
-  button("SPC / u",   icons.packageDown .. " " .. "Update Plugins",     "<cmd>Lazy update<CR>",                                         {}),
-  button("SPC / i",   icons.package .. " " .. "Manage Plugins",         "<cmd>Lazy<CR>",                                                {}),
-  button("SPC / c",   icons.cog .. " " .. "Settings",                   "<cmd>e $MYVIMRC<CR>",                                          {}),
-  button("-",         icons.exit .. " " .. "Exit",                      "<cmd>exit<CR>",                                                {}),
+  button("w",     icons.word .. " " .. "Find Word",                 "<cmd>lua require('plugins.telescope.pickers.multi-rg')()<CR>", {}),
+  button("h",   icons.fileRecent .. " " .. "Recents",             "<cmd>Telescope oldfiles hidden=true<CR>",                      {}),
+  button("l", icons.timer .. " " .. "Load Current Dir Session", "<cmd>SessionManager load_current_dir_session<CR>",             {}),
+  button("u",   icons.packageDown .. " " .. "Update Plugins",     "<cmd>Lazy update<CR>",                                         {}),
+  button("m",   icons.package .. " " .. "Manage Plugins",         "<cmd>Lazy<CR>",                                                {}),
+  button("s",   icons.cog .. " " .. "Settings",                   "<cmd>e $MYVIMRC<CR>",                                          {}),
+  button("<ESC>",         icons.exit .. " " .. "Exit",                      "<cmd>exit<CR>",                                                {}),
 }
 
 -- ╭──────────────────────────────────────────────────────────╮
