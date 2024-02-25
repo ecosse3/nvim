0e970d95b71a279dd50f5b26d5fee9012ff3316f
ecosse3 <luk.kurpiewski@gmail.com>
2024-02-25 11:20:25 +0100

    feat(plugins): add nvim-scissors plugin for snippet management

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -585,6 +585,54 @@ return {
 
   -- Snippets & Language & Syntax
   {
+    "chrisgrieser/nvim-scissors",
+    dependencies = "nvim-telescope/telescope.nvim", -- optional
+    opts = {
+      snippetDir = vim.fn.stdpath("config") .. "/snippets",
+    },
+    keys = {
+      "<Leader>asa",
+      "<Leader>ase",
+    },
+    config = function()
+      local present, wk = pcall(require, "which-key")
+      if not present then
+        return
+      end
+
+      wk.register({
+        a = {
+          s = {
+            name = "Snippets",
+            a = { '<cmd>lua require("scissors").addNewSnippet()<CR>', 'Add new snippet' },
+            e = { '<cmd>lua require("scissors").editSnippet()<CR>', 'Edit snippet' },
+          }
+        }
+      }, {
+        mode = "n",     -- NORMAL mode
+        prefix = "<leader>",
+        silent = true,  -- use `silent` when creating keymaps
+        noremap = true, -- use `noremap` when creating keymaps
+        nowait = false, -- use `nowait` when creating keymaps
+      })
+
+      wk.register({
+        a = {
+          s = {
+            name = "Snippets",
+            a = { '<cmd>lua require("scissors").addNewSnippet()<CR>', 'Add new snippet from selection' },
+          }
+        }
+      }, {
+        mode = "x",     -- VISUAL mode
+        prefix = "<leader>",
+        silent = true,  -- use `silent` when creating keymaps
+        noremap = true, -- use `noremap` when creating keymaps
+        nowait = false, -- use `nowait` when creating keymaps
+      })
+    end
+  },
+  {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
     config = function()
