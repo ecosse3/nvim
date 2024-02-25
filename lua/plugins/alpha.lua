0610a4ed4fa03305661e526b322a8f14787d6d56
ecosse3 <luk.kurpiewski@gmail.com>
2024-02-25 11:32:09 +0100

    feat(plugins): simplify alpha keymappings

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -111,14 +111,14 @@ local function button(sc, txt, keybind, keybind_opts)
 end
 
 dashboard.section.buttons.val = {
-  button("<C-P>",     icons.fileNoBg .. " " .. "Find File",             "<cmd>lua require('plugins.telescope').project_files()<CR>",    {}),
-  button("w",     icons.word .. " " .. "Find Word",                 "<cmd>lua require('plugins.telescope.pickers.multi-rg')()<CR>", {}),
-  button("h",   icons.fileRecent .. " " .. "Recents",             "<cmd>Telescope oldfiles hidden=true<CR>",                      {}),
-  button("l", icons.timer .. " " .. "Load Current Dir Session", "<cmd>SessionManager load_current_dir_session<CR>",             {}),
-  button("u",   icons.packageDown .. " " .. "Update Plugins",     "<cmd>Lazy update<CR>",                                         {}),
-  button("m",   icons.package .. " " .. "Manage Plugins",         "<cmd>Lazy<CR>",                                                {}),
-  button("s",   icons.cog .. " " .. "Settings",                   "<cmd>e $MYVIMRC<CR>",                                          {}),
-  button("<ESC>",         icons.exit .. " " .. "Exit",                      "<cmd>exit<CR>",                                                {}),
+  button("f",     icons.fileNoBg .. " " .. "Find File",             "<cmd>Telescope find_files<CR>",                                       {}),
+  button("w",     icons.word .. " " .. "Find Word",                 "<cmd>lua require('plugins.telescope.pickers.multi-rg')({ layout_strategy = 'vertical' })<CR>", {}),
+  button("h",     icons.fileRecent .. " " .. "Recents",             "<cmd>Telescope oldfiles hidden=true<CR>",                                                      {}),
+  button("l",     icons.timer .. " " .. "Load Current Dir Session", "<cmd>SessionManager load_current_dir_session<CR>",                                             {}),
+  button("u",     icons.packageDown .. " " .. "Update Plugins",     "<cmd>Lazy update<CR>",                                                                         {}),
+  button("m",     icons.package .. " " .. "Manage Plugins",         "<cmd>Lazy<CR>",                                                                                {}),
+  button("s",     icons.cog .. " " .. "Settings",                   "<cmd>e $MYVIMRC<CR>",                                                                          {}),
+  button("<ESC>", icons.exit .. " " .. "Exit",                      "<cmd>exit<CR>",                                                                                {}),
 }
 
 -- ╭──────────────────────────────────────────────────────────╮
