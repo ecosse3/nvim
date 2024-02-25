2db797a8f425e0d65a6f29b82012040b7cf5c477
ecosse3 <luk.kurpiewski@gmail.com>
2024-02-25 11:26:51 +0100

    refactor(functions): extract format function which uses lsp format or eslint

--- lua/config/lsp/functions.lua
+++ lua/config/lsp/functions.lua
@@ -1,30 +1,32 @@
 local M = {}
 
+function M.format()
+  local root_dir = vim.fn.getcwd() -- Adjust this if you have a more accurate way to find the project root
+  local eslintrc_json = root_dir .. "/.eslintrc.json"
+  local eslintrc_js = root_dir .. "/.eslintrc.js"
+
+  -- Check if eslint LSP is active
+  local active_clients = vim.lsp.buf_get_clients()
+  local eslint_is_active = false
+
+  for _, client in ipairs(active_clients) do
+    if client.name == "eslint" then
+      eslint_is_active = true
+      break
+    end
+  end
+
+  if eslint_is_active and (vim.fn.filereadable(eslintrc_json) == 1 or vim.fn.filereadable(eslintrc_js) == 1) then
+    vim.cmd("EslintFixAll")
+  else
+    vim.lsp.buf.format({ async = true })
+  end
+end
+
 function M.enable_format_on_save()
   local group = vim.api.nvim_create_augroup("format_on_save", { clear = false })
   vim.api.nvim_create_autocmd("BufWritePre", {
-    callback = function()
-      local root_dir = vim.fn.getcwd() -- Adjust this if you have a more accurate way to find the project root
-      local eslintrc_json = root_dir .. "/.eslintrc.json"
-      local eslintrc_js = root_dir .. "/.eslintrc.js"
-
-      -- Check if eslint LSP is active
-      local active_clients = vim.lsp.buf_get_clients()
-      local eslint_is_active = false
-
-      for _, client in ipairs(active_clients) do
-        if client.name == "eslint" then
-          eslint_is_active = true
-          break
-        end
-      end
-
-      if eslint_is_active and (vim.fn.filereadable(eslintrc_json) == 1 or vim.fn.filereadable(eslintrc_js) == 1) then
-        vim.cmd("EslintFixAll")
-      else
-        vim.lsp.buf.format()
-      end
-    end,
+    callback = M.format(),
     group = group,
   })
   require("notify")("Enabled format on save", "info", { title = "LSP", timeout = 2000 })
