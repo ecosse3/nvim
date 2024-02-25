fe5f67ce20199c6cffeec56d342e3d47523a27ac
ecosse3 <luk.kurpiewski@gmail.com>
2024-02-25 11:55:28 +0100

    feat(plugins): add remote flash keybinding

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -394,13 +394,8 @@ return {
       }
     },
     keys = {
-      {
-        "s",
-        mode = { "n", "x", "o" },
-        function()
-          require("flash").jump()
-        end,
-      },
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,   desc = "Flash" },
+      { "r", mode = "o",               function() require("flash").remote() end, desc = "Remote Flash" },
     },
   },
   {
