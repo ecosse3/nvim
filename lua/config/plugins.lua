bc6fc642c0f321ba132d19d48c81f59f1115ec68
ecosse3 <luk.kurpiewski@gmail.com>
2024-02-25 11:55:07 +0100

    chore: change commentbox style

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -306,8 +306,8 @@ return {
     "LudoPinelli/comment-box.nvim",
     lazy = false,
     keys = {
-      { "<leader>ac", "<cmd>lua require('comment-box').lbox()<CR>", desc = "comment box" },
-      { "<leader>ac", "<cmd>lua require('comment-box').lbox()<CR>", mode = "v",          desc = "comment box" },
+      { "<leader>ac", "<cmd>lua require('comment-box').llbox()<CR>", desc = "comment box" },
+      { "<leader>ac", "<cmd>lua require('comment-box').llbox()<CR>", mode = "v",          desc = "comment box" },
     }
   },
   {
