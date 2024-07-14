dbf2d2924d3702510da401dc5e041b55fcf1a976
ecosse3 <luk.kurpiewski@gmail.com>
2024-07-14 22:20:16 +0200

    refactor(plugins)!: setup for package-info.nvim

--- lua/plugins/package-info.lua
+++ lua/plugins/package-info.lua
@@ -1,20 +1,23 @@
 local icons = EcoVim.icons
 
-require('package-info').setup
-    {
+return {
+  {
+    "vuki656/package-info.nvim",
+    event = "BufEnter package.json",
+    opts = {
       colors = {
         up_to_date = "#3C4048", -- Text color for up to date package virtual text
         outdated = "#fc514e", -- Text color for outdated package virtual text
       },
       icons = {
-        enable = true, -- Whether to display icons
+        enable = true,                -- Whether to display icons
         style = {
           up_to_date = icons.checkSquare, -- Icon for up to date packages
           outdated = icons.gitRemove, -- Icon for outdated packages
         },
       },
-      autostart = true, -- Whether to autostart when `package.json` is opened
-      hide_up_to_date = true, -- It hides up to date versions when displaying virtual text
+      autostart = true,          -- Whether to autostart when `package.json` is opened
+      hide_up_to_date = true,    -- It hides up to date versions when displaying virtual text
       hide_unstable_versions = true, -- It hides unstable versions from version list e.g next-11.1.3-canary3
 
       -- Can be `npm` or `yarn`. Used for `delete`, `install` etc...
@@ -23,5 +26,5 @@ require('package-info').setup
       -- provided one,                              if nothing is provided it will use `yarn`
       package_manager = 'yarn'
     }
-
--- Keymappings are set in which key config
+  },
+}
