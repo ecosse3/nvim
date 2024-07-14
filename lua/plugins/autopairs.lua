619c25dae3b2ddb338878c5c7c4f5cdd1a292791
ecosse3 <luk.kurpiewski@gmail.com>
2024-07-14 22:21:05 +0200

    feat(plugins)!: add new ultimate-autopairs configuration

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,12 +1,29 @@
-require('nvim-autopairs').setup({
-  check_ts = true,
-  ts_config = {
-    lua = { 'string' },
-    javascript = { 'template_string' },
-    java = false,
-  }
-})
+return {
+  {
+    'altermo/ultimate-autopair.nvim',
+    event = { 'InsertEnter', 'CmdlineEnter' },
+    branch = 'v0.6', --recommended as each new version will have breaking changes
+    opts = {
+      --Config goes here
+    },
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = function()
+      local npairs = require('nvim-autopairs')
+      npairs.setup()
 
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp = require('cmp')
-cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local cmp = require('cmp')
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done()
+      )
+
+      for _, i in ipairs(npairs.config.rules) do
+        i.key_map = nil
+      end
+    end
+  }
+}
