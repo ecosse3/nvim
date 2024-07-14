8e84f5fe73738217e46dc1b3ce3b5b79978ccfab
ecosse3 <luk.kurpiewski@gmail.com>
2024-07-14 22:16:04 +0200

    refactor(plugins)!: spider.lua plugin configuration

---
+++ lua/plugins/spider.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "chrisgrieser/nvim-spider",
+    cond = EcoVim.plugins.jump_by_subwords.enabled,
+    lazy = true,
+    keys = { "w", "e", "b", "ge" },
+    config = function()
+      vim.keymap.set({ "n", "o", "x" }, "W", "w", { desc = "Normal w" })
+      vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
+      vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
+      vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
+      vim.keymap.set(
+        { "n", "o", "x" },
+        "ge",
+        "<cmd>lua require('spider').motion('ge')<CR>",
+        { desc = "Spider-ge" }
+      )
+    end,
+  },
+}
