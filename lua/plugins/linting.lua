8c60d74244cce10a6f7b0c234de51f633f2154b8
ecosse3 <luk.kurpiewski@gmail.com>
2024-07-21 23:22:47 +0200

    feat(plugins): replace null-ls with conform & nvim-lint

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -1,14 +1,25 @@
-local lint = require("lint")
+return {
+  {
+    "mfussenegger/nvim-lint",
+    event = {
+      "BufReadPre",
+      "BufNewFile",
+    },
+    config = function()
+      local lint = require("lint")
 
-lint.linters_by_ft = {
-  javascript = { "eslint_d" },
-  typescript = { "eslint_d" },
-  javascriptreact = { "eslint_d" },
-  typescriptreact = { "eslint_d" },
-  svelte = { "eslint_d" },
-  python = { "pylint" },
+      lint.linters_by_ft = {
+        javascript = { "eslint_d" },
+        typescript = { "eslint_d" },
+        javascriptreact = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
+        svelte = { "eslint_d" },
+        python = { "pylint" },
+      }
+
+      vim.keymap.set("n", "<leader>l", function()
+        lint.try_lint()
+      end, { desc = "lint file" })
+    end,
+  },
 }
-
-vim.keymap.set("n", "<leader>l", function()
-  lint.try_lint()
-end, { desc = "lint file" })
