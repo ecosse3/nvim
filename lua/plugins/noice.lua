91d25cf143b410632c303c2c78ec703c0e34c513
ecosse3 <luk.kurpiewski@gmail.com>
2024-07-21 23:23:00 +0200

    feat(plugins): update noice config

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,25 +1,32 @@
-require("noice").setup({
-  messages = { enabled = false },
-  lsp = {
-    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-    override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
-      ["vim.lsp.util.stylize_markdown"] = false,
-      ["cmp.entry.get_documentation"] = true,
-    },
-    progress = {
-      enabled = false,
-    },
-    hover = {
-      enabled = false,
-    },
+return {
+  {
+    "folke/noice.nvim",
+    cond = EcoVim.plugins.experimental_noice.enabled,
+    lazy = false,
+    opts = {
+      messages = { enabled = false },
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+          ["vim.lsp.util.stylize_markdown"] = false,
+          ["cmp.entry.get_documentation"] = true,
+        },
+        progress = {
+          enabled = false,
+        },
+        hover = {
+          enabled = false,
+        },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = false,    -- use a classic bottom cmdline for search
+        command_palette = true,   -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false,       -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false,   -- add a border to hover docs and signature help
+      },
+    }
   },
-  -- you can enable a preset for easier configuration
-  presets = {
-    bottom_search = false,        -- use a classic bottom cmdline for search
-    command_palette = true,       -- position the cmdline and popupmenu together
-    long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = false,       -- add a border to hover docs and signature help
-  },
-})
+}
