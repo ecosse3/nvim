44350b0740910c1d0d2b034d74a38aef262fa332
ecosse3 <luk.kurpiewski@gmail.com>
2024-08-23 20:37:53 +0200

    refactor(plugins): restructure and enhance indentation plugin configuration

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,19 +1,27 @@
--- https://github.com/lukas-reineke/indent-blankline.nvim
+return {
 
-vim.opt.list = true
--- vim.opt.listchars:append("space:⋅")
--- vim.opt.listchars:append("eol:↴")
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = "BufReadPre",
+    main = "ibl",
+    config = function()
+      vim.opt.list = true
+      -- vim.opt.listchars:append("space:⋅")
+      -- vim.opt.listchars:append("eol:↴")
 
-require("ibl").setup {
-  exclude = {
-    filetypes = { "help", "dashboard", "packer", "NvimTree", "Trouble", "TelescopePrompt", "Float" },
-    buftypes = { "terminal", "nofile", "telescope" },
+      require("ibl").setup {
+        exclude = {
+          filetypes = { "help", "dashboard", "packer", "NvimTree", "Trouble", "TelescopePrompt", "Float" },
+          buftypes = { "terminal", "nofile", "telescope" },
+        },
+        indent = {
+          char = "│",
+        },
+        scope = {
+          enabled = true,
+          show_start = false,
+        }
+      }
+    end,
   },
-  indent = {
-    char = "│",
-  },
-  scope = {
-    enabled = true,
-    show_start = false,
-  }
 }
