c3cfd042848577f287f069e03a9856e9438f60cc
ecosse3 <luk.kurpiewski@gmail.com>
2024-09-06 22:08:54 +0200

    feat: include markdown as a recognized file type for rendering

--- lua/plugins/languages/markdown.lua
+++ lua/plugins/languages/markdown.lua
@@ -2,6 +2,7 @@ return {
   { "dhruvasagar/vim-table-mode", ft = { "markdown" } },
   {
     "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown" },
     opts = {},
     -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
     -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
