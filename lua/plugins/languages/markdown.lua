e0a359870ab3faab84cba89297d5f059c41c62c1
ecosse3 <luk.kurpiewski@gmail.com>
2024-12-20 19:08:07 +0100

    feat: enhance markdown file type support in render markdown plugin

--- lua/plugins/languages/markdown.lua
+++ lua/plugins/languages/markdown.lua
@@ -2,10 +2,22 @@ return {
   { "dhruvasagar/vim-table-mode", ft = { "markdown" } },
   {
     "MeanderingProgrammer/render-markdown.nvim",
-    ft = { "markdown" },
-    opts = {},
-    -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-    -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
+    ft = { "markdown", "Avante", "copilot-chat", "codecompanion" },
+    opts = {
+      render_modes = true,
+      file_types = { "markdown", "Avante", "copilot-chat", "codecompanion" },
+      sign = {
+        enabled = false,
+      },
+    },
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-  }
-}
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && pnpm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  }, }
