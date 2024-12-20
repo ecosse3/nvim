4c08c2ed441d529262f10cb90be294bf1431da62
ecosse3 <luk.kurpiewski@gmail.com>
2024-12-20 18:57:38 +0100

    feat(plugins): add nvim-sops plugin

---
+++ lua/plugins/sops.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'lucidph3nx/nvim-sops',
+    event = { 'BufEnter' },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  }
+}
