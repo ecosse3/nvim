c6241972b36068f995bacdfda6f4240c8b1779b3
ecosse3 <luk.kurpiewski@gmail.com>
2024-12-20 18:58:48 +0100

    fix(plugins): configuration of multicursors

--- lua/plugins/multicursors.lua
+++ lua/plugins/multicursors.lua
@@ -3,24 +3,20 @@ return {
     "smoka7/multicursors.nvim",
     event = "VeryLazy",
     dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'smoka7/hydra.nvim',
+      "nvim-treesitter/nvim-treesitter",
+      "Cathyprime/hydra.nvim"
     },
     opts = {
-      hint_config = {
-        border = EcoVim.ui.float.border or "rounded",
-        position = 'bottom',
-        show_name = false,
-      }
     },
+    cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
     keys = {
       {
-        '<LEADER>m',
+        '<Leader>m',
         '<CMD>MCstart<CR>',
         desc = 'multicursor',
       },
       {
-        '<LEADER>m',
+        '<Leader>m',
         '<CMD>MCvisual<CR>',
         mode = "v",
         desc = 'multicursor',
