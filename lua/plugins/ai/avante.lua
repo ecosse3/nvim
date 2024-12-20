5cef9f0787221d64ca259521886dde20557d7883
ecosse3 <luk.kurpiewski@gmail.com>
2024-12-20 19:09:58 +0100

    feat(plugins): add Avante AI plugin (disabled by default)

---
+++ lua/plugins/ai/avante.lua
@@ -0,0 +1,59 @@
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    enabled = false,
+    lazy = false,
+    version = false,
+    opts = {
+      provider = "copilot",
+      copilot = {
+        model = "claude-3.5-sonnet"
+      },
+      auto_suggestions_provider = "copilot",
+      behaviour = {
+        auto_suggestions = false, -- Experimental stage
+        auto_set_highlight_group = true,
+        auto_set_keymaps = true,
+        auto_apply_diff_after_generation = false,
+        support_paste_from_clipboard = true,
+        minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
+      },
+      hints = { enabled = true },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "zbirenbaum/copilot.lua",
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  }
+}
