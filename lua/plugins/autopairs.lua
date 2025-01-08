e2db8ae87203ea4135b6b709e20c292d05088eea
ecosse3 <luk.kurpiewski@gmail.com>
2025-01-09 00:28:40 +0100

    fix(plugins): working autopairs with blink.cmp

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -7,4 +7,17 @@ return {
       --Config goes here
     },
   },
+  {
+    'windwp/nvim-autopairs',
+    enabled = true,
+    event = "InsertEnter",
+    config = function()
+      local npairs = require('nvim-autopairs')
+      npairs.setup()
+
+      for _, i in ipairs(npairs.config.rules) do
+        i.key_map = nil
+      end
+    end
+  }
 }
