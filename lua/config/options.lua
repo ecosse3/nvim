8cfb1fa136b3b12301885ba09434f01a9563f192
ecosse3 <luk.kurpiewski@gmail.com>
2025-02-04 19:59:31 +0100

    style: incrase neovide transparency a bit

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -86,7 +86,7 @@ if vim.g.neovide then
   vim.g.neovide_window_blurred = true
   vim.g.neovide_floating_blur_amount_x = 2.0
   vim.g.neovide_floating_blur_amount_y = 2.0
-  vim.g.neovide_transparency = 0.9
+  vim.g.neovide_transparency = 0.95
 
   vim.keymap.set({ 'n', 'v' }, '<D-c>', '"+y')  -- Copy
   vim.keymap.set({ 'n', 'v' }, '<D-v>', '"*p')  -- Paste normal/visual mode
