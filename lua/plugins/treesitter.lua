c23fd2be90358e78ea4b04ffbf4fd7d22da99401
ecosse3 <luk.kurpiewski@gmail.com>
2025-02-04 19:55:09 +0100

    feat(plugins): add treesitter context indent plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -129,4 +129,12 @@ return {
     end
 
   },
+
+  {
+    "wurli/contextindent.nvim",
+    -- This is the only config option; you can use it to restrict the files
+    -- which this plugin will affect (see :help autocommand-pattern).
+    opts = { pattern = "*" },
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+  },
 }
