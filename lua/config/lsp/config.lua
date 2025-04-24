4ec30e9086ee050079d11f328cc19e6cac3cb06c
ecosse3 <luk.kurpiewski@gmail.com>
2025-04-25 00:05:10 +0200

    feat(lsp): add auto-floating diagnostics and disable in insert mode

--- lua/config/lsp/config.lua
+++ lua/config/lsp/config.lua
@@ -124,6 +124,33 @@ vim.diagnostic.config({
   },
 })
 
+local group = vim.api.nvim_create_augroup('OoO', {})
+
+local function au(typ, pattern, cmdOrFn)
+	if type(cmdOrFn) == 'function' then
+		vim.api.nvim_create_autocmd(typ, { pattern = pattern, callback = cmdOrFn, group = group })
+	else
+		vim.api.nvim_create_autocmd(typ, { pattern = pattern, command = cmdOrFn, group = group })
+	end
+end
+
+au({ 'CursorHold', 'InsertLeave' }, nil, function()
+	local opts = {
+		focusable = false,
+		scope = 'cursor',
+		close_events = { 'BufLeave', 'CursorMoved', 'InsertEnter' },
+	}
+	vim.diagnostic.open_float(nil, opts)
+end)
+
+au('InsertEnter', nil, function()
+	vim.diagnostic.enable(false)
+end)
+
+au('InsertLeave', nil, function()
+	vim.diagnostic.enable(true)
+end)
+
 -- UI
 
 local lspui_ok, lspui = pcall(require, 'lspconfig.ui.windows')
