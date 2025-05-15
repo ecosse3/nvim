95619baa978451191f81dac61a8688b64430d6c7
ecosse3 <luk.kurpiewski@gmail.com>
2025-05-15 23:44:57 +0200

    feat(plugins): add NES completion to move cursor after insertion with blink.cmp

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -51,7 +51,10 @@ return {
           function(cmp)
             if vim.b[vim.api.nvim_get_current_buf()].nes_state then
               cmp.hide()
-              return require("copilot-lsp.nes").apply_pending_nes()
+              return (
+                require("copilot-lsp.nes").apply_pending_nes()
+                and require("copilot-lsp.nes").walk_cursor_end_edit()
+              )
             end
             if cmp.snippet_active() then
               return cmp.accept()
