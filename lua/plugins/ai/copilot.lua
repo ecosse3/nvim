60f5de7623c7ab557b13cd0882d2c9d66a5cf7d3
ecosse3 <luk.kurpiewski@gmail.com>
2025-05-15 23:43:00 +0200

    feat(ai): improve Copilot keybinding to navigate suggestion edits & update to native vim lsp

--- lua/plugins/ai/copilot.lua
+++ lua/plugins/ai/copilot.lua
@@ -24,10 +24,15 @@ return {
   {
     "copilotlsp-nvim/copilot-lsp",
     init = function()
-      vim.g.copilot_nes_debounce = 500
-      vim.lsp.enable("copilot")
+      vim.g.copilot_nes_debounce = 300
+      vim.lsp.enable("copilot_ls")
       vim.keymap.set("n", "<tab>", function()
-        require("copilot-lsp.nes").apply_pending_nes()
+        -- Try to jump to the start of the suggestion edit.
+        -- If already at the start, then apply the pending suggestion and jump to the end of the edit.
+        local _ = require("copilot-lsp.nes").walk_cursor_start_edit()
+            or (
+              require("copilot-lsp.nes").apply_pending_nes() and require("copilot-lsp.nes").walk_cursor_end_edit()
+            )
       end)
     end,
   }
