6f809285f416c24eb174e3f7f8ccd5800b897236
ecosse3 <luk.kurpiewski@gmail.com>
2026-02-28 21:46:37 +0100

    feat(keymaps): add macOS Cmd key mappings
    - Add <D-i> for jumplist forward
    - Add <D-d>/<D-u> for half-page scroll
    - Add <D-s> for save file
    - Add <D-q>/<D-w> for buffer delete
    - Add <F19> for previous buffer
    - Add <BS> for beginning of line
    - Comment out SpeedDating fallback mappings

--- lua/config/keymappings.lua
+++ lua/config/keymappings.lua
@@ -2,8 +2,13 @@ local keymap = vim.keymap.set
 local silent = { silent = true }
 
 table.unpack = table.unpack or unpack -- 5.1 compatibility
--- Fix moving forward in jumplist via <C-i>
+-- Fix moving forward in jumplist via <C-i> and <D-i>
 keymap("n", "<C-I>", "<C-I>", silent)
+keymap("n", "<D-I>", "<C-I>", silent)
+
+-- Add down/up mapping for Cmd
+keymap("n", "<D-d>", "<C-d>", silent)
+keymap("n", "<D-u>", "<C-u>", silent)
 
 -- H to move to the first non-blank character of the line
 keymap("n", "H", "^", silent)
@@ -20,9 +25,14 @@ keymap("v", ">", ">gv", silent)
 keymap("v", "`", "u", silent)
 keymap("v", "<A-`>", "U", silent)
 
--- Save file by CTRL-S
+-- Jump to beginning with backspace
+keymap("n", "<BS>", "^", silent)
+
+-- Save file by CTRL-S/CMD-S
 keymap("n", "<C-s>", ":w<CR>", silent)
+keymap("n", "<D-s>", ":w<CR>", silent)
 keymap("i", "<C-s>", "<ESC> :w<CR>", silent)
+keymap("i", "<D-s>", "<ESC> :w<CR>", silent)
 
 -- Remove highlights
 keymap("n", "<CR>", ":noh<CR><CR>", silent)
@@ -31,8 +41,11 @@ keymap("n", "<CR>", ":noh<CR><CR>", silent)
 keymap("n", "<Tab>", ":BufferLineCycleNext<CR>", silent)
 keymap("n", "gn", ":bn<CR>", silent)
 keymap("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", silent)
+keymap("n", "<F19>", ":BufferLineCyclePrev<CR>", silent)
 keymap("n", "gp", ":bp<CR>", silent)
 keymap("n", "<S-q>", ":lua Snacks.bufdelete()<CR>", silent)
+keymap("n", "<D-q>", ":lua Snacks.bufdelete()<CR>", silent)
+keymap("n", "<D-w>", ":lua Snacks.bufdelete()<CR>", silent)
 
 -- Don't yank on delete char
 keymap("n", "x", '"_x', silent)
@@ -44,10 +57,10 @@ keymap("v", "X", '"_X', silent)
 keymap("v", "p", '"_dP', silent)
 
 -- Avoid issues because of remapping <c-a> and <c-x> below
-vim.cmd([[
-  nnoremap <Plug>SpeedDatingFallbackUp <c-a>
-  nnoremap <Plug>SpeedDatingFallbackDown <c-x>
-]])
+-- vim.cmd([[
+--   nnoremap <Plug>SpeedDatingFallbackUp <c-a>
+--   nnoremap <Plug>SpeedDatingFallbackDown <c-x>
+-- ]])
 
 -- Quickfix
 keymap("n", "<Leader>,", ":cp<CR>", silent)
@@ -57,9 +70,9 @@ keymap("n", "<Leader>.", ":cn<CR>", silent)
 keymap("n", "<leader>q", "<cmd>lua require('utils').toggle_quicklist()<CR>", silent)
 
 -- Manually invoke speeddating in case switch.vim didn't work
-keymap("n", "<C-a>", ":if !switch#Switch() <bar> call speeddating#increment(v:count1) <bar> endif<CR>", silent)
-keymap("n", "<C-x>", ":if !switch#Switch({'reverse': 1}) <bar> call speeddating#increment(-v:count1) <bar> endif<CR>",
-  silent)
+-- keymap("n", "<C-a>", ":if !switch#Switch() <bar> call speeddating#increment(v:count1) <bar> endif<CR>", silent)
+-- keymap("n", "<C-x>", ":if !switch#Switch({'reverse': 1}) <bar> call speeddating#increment(-v:count1) <bar> endif<CR>",
+--   silent)
 
 -- Open links under cursor in browser with gx
 if vim.fn.has('macunix') == 1 then
