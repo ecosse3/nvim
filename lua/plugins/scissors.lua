073620605e4d27c8afd5813fa5edca82d378c238
ecosse3 <luk.kurpiewski@gmail.com>
2026-03-03 23:35:34 +0100

    fix(scissors): use opts instead of config that skipped setup
    The config function registered which-key keymaps but never called
    scissors.setup(), so opts (snippetDir) was never applied. Replace
    config with proper lazy.nvim keys spec and let opts handle setup.
    Also removes eager BufEnter loading in favor of key-based lazy load.

--- lua/plugins/scissors.lua
+++ lua/plugins/scissors.lua
@@ -1,31 +1,14 @@
 return {
   {
     "chrisgrieser/nvim-scissors",
-    event = "BufEnter",
     dependencies = "nvim-telescope/telescope.nvim",
     opts = {
       snippetDir = vim.fn.stdpath("config") .. "/snippets",
     },
     keys = {
-      "<Leader>asa",
-      "<Leader>ase",
+      { "<leader>asa", '<cmd>lua require("scissors").addNewSnippet()<CR>', desc = "Add new snippet" },
+      { "<leader>ase", '<cmd>lua require("scissors").editSnippet()<CR>', desc = "Edit snippet" },
+      { "<leader>asa", '<cmd>lua require("scissors").addNewSnippet()<CR>', desc = "Add new snippet from selection", mode = "x" },
     },
-    config = function()
-      local present, wk = pcall(require, "which-key")
-      if not present then
-        return
-      end
-
-      wk.add({
-        { "<leader>as",  group = "Snippets",                                 nowait = false,           remap = false },
-        { "<leader>asa", '<cmd>lua require("scissors").addNewSnippet()<CR>', desc = "Add new snippet", nowait = false, remap = false },
-        { "<leader>ase", '<cmd>lua require("scissors").editSnippet()<CR>',   desc = "Edit snippet",    nowait = false, remap = false },
-      })
-
-      wk.add({
-        { "<leader>as",  group = "Snippets",                                 mode = "x",                              nowait = false, remap = false },
-        { "<leader>asa", '<cmd>lua require("scissors").addNewSnippet()<CR>', desc = "Add new snippet from selection", mode = "x",     nowait = false, remap = false },
-      })
-    end
   }
 }
