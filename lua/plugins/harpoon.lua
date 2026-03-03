1d3a243fc765c0795a0d3263a22dabaef8c41265
ecosse3 <luk.kurpiewski@gmail.com>
2026-03-03 23:31:09 +0100

    perf(harpoon): lazy load on keymaps instead of startup
    Move keymaps to lazy.nvim keys spec so harpoon only loads when first
    keymap is pressed.

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -3,22 +3,18 @@ return {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<Leader>H", function() require("harpoon"):list():add() end, desc = "Harpoon add" },
+      { "<Leader>h", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end, desc = "Harpoon menu" },
+      { "<Leader>1", function() require("harpoon"):list():select(1) end, desc = "Harpoon 1" },
+      { "<Leader>2", function() require("harpoon"):list():select(2) end, desc = "Harpoon 2" },
+      { "<Leader>3", function() require("harpoon"):list():select(3) end, desc = "Harpoon 3" },
+      { "<Leader>4", function() require("harpoon"):list():select(4) end, desc = "Harpoon 4" },
+      { "<Leader>[", function() require("harpoon"):list():prev() end, desc = "Harpoon prev" },
+      { "<Leader>]", function() require("harpoon"):list():next() end, desc = "Harpoon next" },
+    },
     config = function()
-      local harpoon = require("harpoon")
-
-      harpoon:setup()
-
-      vim.keymap.set("n", "<Leader>H", function() harpoon:list():add() end)
-      vim.keymap.set("n", "<Leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-      vim.keymap.set("n", "<Leader>1", function() harpoon:list():select(1) end)
-      vim.keymap.set("n", "<Leader>2", function() harpoon:list():select(2) end)
-      vim.keymap.set("n", "<Leader>3", function() harpoon:list():select(3) end)
-      vim.keymap.set("n", "<Leader>4", function() harpoon:list():select(4) end)
-
-      -- Toggle previous & next buffers stored within Harpoon list
-      vim.keymap.set("n", "<Leader>[", function() harpoon:list():prev() end)
-      vim.keymap.set("n", "<Leader>]", function() harpoon:list():next() end)
+      require("harpoon"):setup()
     end
   },
 }
