e9a01b08e867ee36fab3cb37d91b647ee1b61c02
ecosse3 <luk.kurpiewski@gmail.com>
2026-03-03 23:27:29 +0100

    fix(todo-comments): correct malformed lazy.nvim keymap format
    The ]t and [t keymaps used positional format { mode, lhs, rhs, {desc} }
    which lazy.nvim does not support. Use the correct named-key format
    { lhs, rhs, mode = ..., desc = ... }.

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -27,11 +27,8 @@ return {
     keys = {
       { "<leader>st", function() Snacks.picker.todo_comments() end,                                          desc = "Todo" },
       { "<leader>sT", function() Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
-      { "n",          "]t",                                                                                  function()
-        require("todo-comments").jump_next() end,                                                                                                                 { desc = "Next todo comment" } },
-      { "n", "[t", function()
-        require("todo-comments").jump_prev()
-      end, { desc = "Previous todo comment" } }
+      { "]t", function() require("todo-comments").jump_next() end, mode = "n", desc = "Next todo comment" },
+      { "[t", function() require("todo-comments").jump_prev() end, mode = "n", desc = "Previous todo comment" }
     }
   }
 }
