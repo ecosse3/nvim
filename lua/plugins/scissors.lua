86800b755e38074d1151b87c4847564e329d60b1
ecosse3 <luk.kurpiewski@gmail.com>
2026-03-04 00:57:35 +0100

    fix(scissors): remove telescope dependency (auto-detects snacks picker)
    nvim-scissors auto-detects the available picker backend. With
    snacks.nvim installed, it will use that instead of telescope.

--- lua/plugins/scissors.lua
+++ lua/plugins/scissors.lua
@@ -1,7 +1,6 @@
 return {
   {
     "chrisgrieser/nvim-scissors",
-    dependencies = "nvim-telescope/telescope.nvim",
     opts = {
       snippetDir = vim.fn.stdpath("config") .. "/snippets",
     },
