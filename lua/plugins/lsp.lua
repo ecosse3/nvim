08b0c492e5fd8988540f4eb7962d4dc60ba29797
ecosse3 <luk.kurpiewski@gmail.com>
2026-03-04 23:45:37 +0100

    fix(plugins): remove nvim-lsp-file-operations (doesn't support snacks explorer)
    The plugin only supports nvim-tree, neo-tree, and triptych as file
    explorers. Since EcoVim uses snacks.nvim explorer, this plugin was
    non-functional. Also removes the orphaned nvim-tree.lua dependency.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -113,16 +113,4 @@ return {
 			{ "<leader>sS", vim.lsp.buf.workspace_symbol, desc = "Workspace Symbols" },
 		},
 	},
-
-	{
-		"antosha417/nvim-lsp-file-operations",
-		event = "LspAttach",
-		dependencies = {
-			{ "nvim-lua/plenary.nvim" },
-			{ "nvim-tree/nvim-tree.lua" },
-		},
-		config = function()
-			require("lsp-file-operations").setup()
-		end,
-	},
 }
