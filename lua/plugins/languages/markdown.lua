0e3709e3f03d662eadb7a395a80eaee34be75513
ecosse3 <luk.kurpiewski@gmail.com>
2026-07-29 00:13:44 +0200

    fix: use yarn for markdown installation

--- lua/plugins/languages/markdown.lua
+++ lua/plugins/languages/markdown.lua
@@ -17,9 +17,10 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && pnpm install",
+    build = "cd app && yarn install",
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
+      vim.g.mkdp_theme = 'light'
     end,
     ft = { "markdown" },
   }, }
